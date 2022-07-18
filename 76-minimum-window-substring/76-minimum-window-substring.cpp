5e012ee2cb29d4630ee39cee5de787aa3b146d2b
Shivam Shukla <19ucs040@lnmiit.ac.in>
2022-07-18 18:18:14 +0530

    Time: 40 ms (38.70%), Space: 16.3 MB (19.25%) - LeetHub

---
+++ 76-minimum-window-substring/76-minimum-window-substring.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        unordered_map<char, int> mp;
+        for(char c: t)
+            mp[c]++;
+        int i, j;
+        int len = INT_MAX;
+        string ans = "";
+        int n = s.size();
+        int uniq= mp.size();
+        for(i = 0, j = 0; j < n; j++){
+            if(mp.find(s[j])!= mp.end()){
+                mp[s[j]]--;
+                if(mp[s[j]] == 0)
+                    uniq--;
+            }
+            // for(auto [k,v] : mp)
+            //     cout << k << " = "<<v << endl;
+            // cout << uniq<<"_____________" << endl;
+            while(i < n && uniq == 0){
+                int currLen = j - i + 1;
+                if(currLen < len){
+                    len = min(currLen, len);
+                    ans = s.substr(i,j-i+1);
+                }
+                if(mp.find(s[i]) != mp.end()){
+                    mp[s[i]]++;
+                    if(mp[s[i]] == 1)
+                        uniq++;
+                }
+                i++;
+            }
+        }
+        return ans;
+    }
+};
