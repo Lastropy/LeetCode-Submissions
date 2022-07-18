fa9cc6a7ea041a4a7787a0dfd2192ec8b0426274
Shivam Shukla <19ucs040@lnmiit.ac.in>
2022-07-18 17:05:33 +0530

    Time: 3 ms (38.93%), Space: 6.6 MB (31.97%) - LeetHub

---
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int n) {
+        vector<vector<int>> ans;
+          ans.push_back(vector<int>({1}));
+        if(n == 1){
+            return ans;
+        }
+        // ans.push_back(vector<int>({1,1}));
+        for(int i  = 1; i < n; i++){
+            vector<int> temp;
+            for(int j = 0; j <= i; j++){
+                int val = 0;
+                if(j > 0)
+                    val += ans[i-1][j-1];
+                if(j <= i-1)
+                    val += ans[i-1][j];
+                temp.push_back(val);
+            }
+            ans.push_back(temp);
+        }
+        return ans;  
+    }
+};
