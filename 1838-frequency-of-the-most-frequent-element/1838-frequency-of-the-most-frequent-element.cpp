53326dfdf2557271e5e3659a120f27f6e8a943c8
Shivam Shukla <19ucs040@lnmiit.ac.in>
2022-07-19 07:28:13 +0530

    Time: 257 ms (91.51%), Space: 99.1 MB (20.93%) - LeetHub

--- 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.cpp
+++ 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.cpp
@@ -4,21 +4,19 @@ public:
         int i = 0, j = 0;
         sort(a.begin(), a.end());
         int n = a.size();
-        long tot = 0;
-        int ans = 1;
+        long tot = 0, ans = 1, winSize = 1;
         for(int i = 1; i < n; i++){
             if(a[i-1] != a[i])
                 break;
             ans++;
         }
-        int winSize = 1;
         for(i = 0, j = 0; j < n ; j++){
             tot += a[j];
             winSize = j- i + 1;
-            if(tot + 1LL * k >= 1LL * a[j] * winSize)
+            if(tot + k >= a[j] * winSize)
                 ans = max(ans, winSize);
             else{
-                while(i < n && tot + k < 1LL * a[j] * winSize){
+                while(i < n && tot + k < a[j] * winSize){
                     tot -= a[i];
                     i++;
                     winSize = j - i + 1;
