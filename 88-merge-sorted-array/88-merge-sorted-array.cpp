4e83475bec5ab3c30fb63c6807447f74b195d42f
Shivam Shukla <19ucs040@lnmiit.ac.in>
2022-07-19 17:11:34 +0530

    Time: 0 ms (100.00%), Space: 9.3 MB (29.13%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -1,9 +1,21 @@
 class Solution {
 public:
     void merge(vector<int>& a, int m, vector<int>& b, int n) {
-       for(int i = m; i < a.size(); i++){
-           a[i] = b[i-m];
-       }
-        sort(a.begin(), a.end());
+        map<int, int> mp;
+        for(int i = 0; i < m; i++){
+            mp[a[i]]++;
+        }
+        
+        for(int j = 0; j < n; j++){
+            mp[b[j]]++;
+        }
+        int pos  = 0;
+        for(auto [k,v]: mp){
+            while(v){
+                a[pos++] = k;
+                v--;
+            }
+        }
+        return;
     }
 };
