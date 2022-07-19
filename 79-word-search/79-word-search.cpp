974b080961872710074cf3e5c02ee8a84ff3a88b
Shivam Shukla <19ucs040@lnmiit.ac.in>
2022-07-19 17:36:17 +0530

    Time: 258 ms (92.88%), Space: 7.8 MB (91.48%) - LeetHub

--- 79-word-search/79-word-search.cpp
+++ 79-word-search/79-word-search.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
     bool find(vector<vector<char>>& board, int i, int j, int st, string &word){
-        if(board[i][j] == '*') return false;
         if(st == word.size()) return true;
-        if(st == word.size() -1  && board[i][j] == word[st]) return true;
-        if(word[st] != board[i][j]) return false;
+        if(i < 0 || i >= board.size() || j < 0 || j >= board[0].size() || word[st] != board[i][j]) return false;
         char c = board[i][j];
         board[i][j] = '*';
-        if( i > 0 && find(board, i-1, j , st+1, word)) return true;
-        if( j > 0 && find(board, i, j-1, st+1, word)) return true;
-        if(j < board[0].size()-1 && find(board,i, j+1, st+1, word)) return true;
-        if(i < board.size()-1 && find(board,i+1, j, st+1, word)) return true;
+        if(find(board, i-1, j , st+1, word)) return true;
+        if(find(board, i, j-1, st+1, word)) return true;
+        if(find(board,i, j+1, st+1, word)) return true;
+        if(find(board,i+1, j, st+1, word)) return true;
         board[i][j] = c;
         return false;
             
