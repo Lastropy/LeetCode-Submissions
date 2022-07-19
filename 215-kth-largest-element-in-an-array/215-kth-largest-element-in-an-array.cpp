b864958b2ccd79682787f7909b597fabe291c937
Shivam Shukla <19ucs040@lnmiit.ac.in>
2022-07-19 14:32:42 +0530

    Time: 134 ms (6.93%), Space: 47.4 MB (8.87%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
@@ -6,15 +6,9 @@ public:
         priority_queue<int, vector<int> , greater<int>> pq;
         for(int i  = 0; i < n; i++){
             pq.push(nums[i]);
-            if(pq.size() > k)
-                pq.pop();
+            if(pq.size() > k) pq.pop();
         }
         int ans = pq.top();
-        // while(!pq.empty()){
-        //     cout << pq.top() << " ";
-        //     ans = pq.top(); pq.pop();
-        // }
-        // cout << endl;
         return ans;
     }
 };
