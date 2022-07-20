7d36f261b114f56c92a3e0ea566867c7718749c1
Shivam Shukla <19ucs040@lnmiit.ac.in>
2022-07-20 19:33:13 +0530

    Time: 695 ms (40.24%), Space: 17.6 MB (34.39%) - LeetHub

--- 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.cpp
+++ 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
-    int leastFreq(map<char, int> &mp){
+    int leastFreq(unordered_map<char, int> &mp){
         int lf = INT_MAX;
         for(auto [k,v]: mp) if(v < lf) lf = v;
         return lf;
     }
     int beautySum(string s) {
-        map<char, int> mp;
+        unordered_map<char, int> mp;
         int ans = 0;
         for(int i = 0; i < s.size(); i++){
             mp.clear();
