f05e8318bcb5fe287df4d4d043706dffc42df425
Shivam Shukla <19ucs040@lnmiit.ac.in>
2022-07-20 11:11:22 +0530

    Time: 208 ms (5.00%), Space: 7.9 MB (97.70%) - LeetHub

---
+++ 451-sort-characters-by-frequency/451-sort-characters-by-frequency.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    string frequencySort(string s) {
+        unordered_map<char, int> mp;
+        for(char c: s)
+            mp[c]++;
+        sort(begin(s), end(s), [&](char a, char b) {
+            return mp[a] == mp[b] ? a > b : mp[a] > mp[b];
+        });
+        return s;
+    }
+};
