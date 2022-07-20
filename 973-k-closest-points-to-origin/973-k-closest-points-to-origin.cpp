38bb12b1f8d3acdf6b3425c8ec9b287f69b33614
Shivam Shukla <19ucs040@lnmiit.ac.in>
2022-07-20 11:26:20 +0530

    Time: 437 ms (39.86%), Space: 82.6 MB (15.89%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
-    double dist(int x, int y){
-        double ans = pow(x,2) + pow(y,2);
-        ans = sqrt(ans);
+    long dist(int x, int y){
+        long ans = pow(x,2) + pow(y,2);
         return ans;
     }
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-        typedef pair<double, pair<int,int>> pdpi;
+        typedef pair<long, vector<int>> pdpi;
         priority_queue<pdpi> pq;
         for(auto pt: points){
-            pq.push({dist(pt[0],pt[1]),{pt[0],pt[1]}});
+            pq.push({dist(pt[0],pt[1]),pt});
             if(pq.size() > k)
                 pq.pop();
         }
         vector<vector<int>> ans;
         while(!pq.empty()){
             auto cord = pq.top().second;
-            int x = cord.first;
-            int y = cord.second;
-            ans.push_back(vector<int>({x,y}));
+            ans.push_back(cord);
             pq.pop();
         }
         return ans;
