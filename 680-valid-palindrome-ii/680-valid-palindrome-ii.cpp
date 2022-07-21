9442cacd34eeda527c2597ac5b53f5e4b8d57327
Shivam Shukla <19ucs040@lnmiit.ac.in>
2022-07-21 18:23:16 +0530

    Time: 73 ms (58.98%), Space: 31.9 MB (5.04%) - LeetHub

---
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool ifPalin(string s){
+        // cout << s << endl;
+        string t = s;
+        reverse(t.begin(), t.end());
+        return s == t;
+    }
+    bool validPalindrome(string s) {
+        int i , j;
+        int n = s.size();
+        int count  = 0;
+        for(i = 0, j = n-1; i < j; i++, j--){
+            if(s[i] == s[j])
+                continue;
+            if(count == 1)
+                return false;
+            return ifPalin(s.substr(0,i)+s.substr(i+1)) || ifPalin(s.substr(0,j) + s.substr(j+1));
+        }
+        return true;
+    }
+};
