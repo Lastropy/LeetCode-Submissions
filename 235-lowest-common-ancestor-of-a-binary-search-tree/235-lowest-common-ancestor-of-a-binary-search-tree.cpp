6e2e3346e2db15d7d97752a2a48b18cf4b692016
Shivam Shukla <19ucs040@lnmiit.ac.in>
2022-07-23 17:00:51 +0530

    Time: 58 ms (21.67%), Space: 23.3 MB (63.67%) - LeetHub

--- 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -10,25 +10,34 @@
 
 class Solution {
 public:
-    bool path(TreeNode *root, TreeNode *p, vector<TreeNode *> &p1){
-        if(root == NULL) return false;
-        p1.push_back(root);
-        if(root -> val == p -> val)
-            return true;
-        else if(path(root -> left, p, p1) || path(root -> right, p ,p1))
-            return true;
-        p1.pop_back();
-        return false;
-    }
+    // bool path(TreeNode *root, TreeNode *p, vector<TreeNode *> &p1){
+    //     if(root == NULL) return false;
+    //     p1.push_back(root);
+    //     if(root -> val == p -> val)
+    //         return true;
+    //     else if(path(root -> left, p, p1) || path(root -> right, p ,p1))
+    //         return true;
+    //     p1.pop_back();
+    //     return false;
+    // }
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q)  {
-        vector<TreeNode *> p1, p2;
-        path(root, p , p1);
-        path(root , q , p2);
-        auto ans = root;
-        for(int i = 0; i < min(p1.size(), p2.size()); i++){
-            if(p1[i] == p2[i])
-                ans = p1[i];
-        }
-        return ans;
+        //  // Solution 1 -> by path. General Solution
+        // vector<TreeNode *> p1, p2;
+        // path(root, p , p1);
+        // path(root , q , p2);
+        // auto ans = root;
+        // for(int i = 0; i < min(p1.size(), p2.size()); i++){
+        //     if(p1[i] == p2[i])
+        //         ans = p1[i];
+        // }
+        // return ans;
+        
+        
+        // Solution 2 -> using property of BST
+        if(root -> val < p-> val && root -> val < q-> val)
+            return lowestCommonAncestor(root -> right, p , q);
+        else if( root -> val > p -> val && root -> val > q -> val)
+            return lowestCommonAncestor(root -> left, p , q);
+        return root;
     }
 };
