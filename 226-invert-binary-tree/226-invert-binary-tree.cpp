31199ec9ce9ad278768f2ffb7a7dbc3bebf1d9f3
Shivam Shukla <19ucs040@lnmiit.ac.in>
2022-07-23 11:14:34 +0530

    Time: 7 ms (24.49%), Space: 9.8 MB (38.60%) - LeetHub

--- 226-invert-binary-tree/226-invert-binary-tree.cpp
+++ 226-invert-binary-tree/226-invert-binary-tree.cpp
@@ -12,12 +12,33 @@
 class Solution {
 public:
     TreeNode* invertTree(TreeNode* root) {
+        // Iterative soln -> 1 stack
+        // TC -> O(n) 
+        // SC -> O(n)
         if(root == NULL) return root;
-        root -> left = invertTree(root -> left);
-        root -> right = invertTree(root -> right);
-        auto temp = root -> left;
-        root -> left = root -> right;
-        root -> right = temp;
+        stack<TreeNode *> st;
+        st.push(root);
+        auto curr = root;
+        while(!st.empty()){
+            auto temp = st.top();
+            st.pop();
+            swap(temp -> right, temp -> left);
+            if(temp -> left) st.push(temp -> left);
+            if(temp -> right) st.push(temp -> right);
+        }
         return root;
+        
+        
+        
+        //  // Recursive approach
+        // // TC -> O(n) 
+        // // SC -> O(n)
+        // if(root == NULL) return root;
+        // root -> left = invertTree(root -> left);
+        // root -> right = invertTree(root -> right);
+        // auto temp = root -> left;
+        // root -> left = root -> right;
+        // root -> right = temp;
+        // return root;
     }
 };
