063cb2bf149cee09945cf5300bb76845f7a77715
Shivam Shukla <19ucs040@lnmiit.ac.in>
2022-07-27 15:15:13 +0530

    Time: 78 ms (39.86%), Space: 17.2 MB (14.93%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
@@ -9,14 +9,27 @@
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        // auto temp2 = headB;
+        // while(temp2){
+        //     auto temp1 = headA;
+        //     while(temp1){
+        //         if(temp1 == temp2)
+        //             return temp1;
+        //         temp1 = temp1 -> next;
+        //     }
+        //     temp2 = temp2 -> next;
+        // }
+        // return NULL;
+        auto temp1 = headA;
+        unordered_set<ListNode *> s;
+        while(temp1){
+            s.insert(temp1);
+            temp1 = temp1 -> next;
+        }
         auto temp2 = headB;
         while(temp2){
-            auto temp1 = headA;
-            while(temp1){
-                if(temp1 == temp2)
-                    return temp1;
-                temp1 = temp1 -> next;
-            }
+            if(s.find(temp2) != s.end())
+                return temp2;
             temp2 = temp2 -> next;
         }
         return NULL;
