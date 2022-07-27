56a2f19937b1e396352362f1ec6f2776add55132
Shivam Shukla <19ucs040@lnmiit.ac.in>
2022-07-27 14:05:01 +0530

    Time: 7 ms (81.54%), Space: 6.3 MB (98.13%) - LeetHub

--- 43-multiply-strings/43-multiply-strings.cpp
+++ 43-multiply-strings/43-multiply-strings.cpp
@@ -2,9 +2,8 @@ class Solution {
 public:
     string multiply(string num1, string num2) {
         if(num1 == "0" || num2 == "0") return "0";
-        // string res = string(num1.size() + num2.size(), '0');
         int tot_size = num1.size() + num2.size();
-        string res = string( tot_size , '0');
+        string res( tot_size , '0');
         for (int i = num1.size() - 1; i >= 0; i--) {
             int carry = 0;
             for (int j = num2.size() - 1; j >= 0; j--) {
