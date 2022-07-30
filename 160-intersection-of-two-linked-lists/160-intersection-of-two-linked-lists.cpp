ee3e8d460ac809bbf001a05b6b338d5ad865bbd9
Shivam Shukla <19ucs040@lnmiit.ac.in>
2022-07-30 14:30:51 +0530

    Time: 47 ms (92.76%), Space: 14.6 MB (72.60%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
@@ -8,7 +8,14 @@
  */
 class Solution {
 public:
+    int len(ListNode* t){
+        if(!t) return 0;
+        return 1 + len(t -> next);
+    }
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        // // Approach 1 -> brute force
+        // // TC -> O(n * m)
+        // // SC -> O (1)
         // auto temp2 = headB;
         // while(temp2){
         //     auto temp1 = headA;
@@ -20,18 +27,41 @@ public:
         //     temp2 = temp2 -> next;
         // }
         // return NULL;
-        auto temp1 = headA;
-        unordered_set<ListNode *> s;
-        while(temp1){
-            s.insert(temp1);
-            temp1 = temp1 -> next;
+        
+        // // Approach2 -> hash set
+        // // TC -> O(n + m)
+        // // SC -> O(n)
+        // auto temp1 = headA;
+        // unordered_set<ListNode *> s;
+        // while(temp1){
+        //     s.insert(temp1);
+        //     temp1 = temp1 -> next;
+        // }
+        // auto temp2 = headB;
+        // while(temp2){
+        //     if(s.find(temp2) != s.end())
+        //         return temp2;
+        //     temp2 = temp2 -> next;
+        // }
+        // return NULL;
+    
+        int len1 = len(headA), len2 = len(headB);
+        if(len1 < len2)
+        {
+            swap(headA, headB);
+            swap(len1, len2);
         }
-        auto temp2 = headB;
-        while(temp2){
-            if(s.find(temp2) != s.end())
-                return temp2;
-            temp2 = temp2 -> next;
+        int diff = len1 - len2;
+        while(diff--){
+            headA = headA -> next;            
+        }
+        while(headA && headB){
+            if(headA == headB)
+                return headA;
+            headA = headA -> next;
+            headB = headB -> next;
         }
         return NULL;
+        
     }
 };
