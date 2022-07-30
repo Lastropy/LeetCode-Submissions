56f0ce159b73b10400711a5cb1cae24f375b36e0
Shivam Shukla <19ucs040@lnmiit.ac.in>
2022-07-30 10:09:32 +0530

    Time: 518 ms (26.74%), Space: 124.8 MB (26.52%) - LeetHub

---
+++ 916-word-subsets/916-word-subsets.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool check(vector<int> f, string s){
+        vector<int> f2(26, 0);
+        for(int i = 0; i < s.size(); i++)
+            f2[s[i]-'a']++;
+        for(int i = 0; i < 26; i++){
+            if(f[i]){
+                if(f2[i] < f[i])
+                    return false;
+            }
+        }
+        return true;
+    }
+    vector<string> wordSubsets(vector<string>& words1, vector<string>& words2) {
+        vector<string> ans;
+        vector<int> gf(26, 0 );
+        for(string w: words2){
+            vector<int> curr(26, 0);
+            for(char c: w)
+                gf[c-'a'] = max(++curr[c - 'a'], gf[c-'a']);
+        }
+        // for(int i = 0; i < 26; i++)
+        //     cout << (char)('a' + i) << " "<< gf[i] << endl; 
+        for(string w: words1){
+            if(check(gf, w))
+                ans.push_back(w);
+        }
+        return ans;
+    }
+};
