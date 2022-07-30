21daca734c9470e48fdd74121fd1d1c22087e23d
Shivam Shukla <19ucs040@lnmiit.ac.in>
2022-07-30 14:08:13 +0530

    Time: 51 ms (64.03%), Space: 70.8 MB (97.65%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.cpp
+++ 2-add-two-numbers/2-add-two-numbers.cpp
@@ -19,24 +19,21 @@ public:
         return cnt;
     }
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        if(!l1) return l2;
-        if(!l2) return l1;
+        if(!l1 || !l2) return l2 ? l2 : l1;
         if(len(l1) < len(l2)) swap(l1, l2);
         int carry = 0;
-        auto p1 = l1, p2  = l2;
-        auto d = p1;
+        auto p1 = l1;
         ListNode* prev = NULL;
-        while(p1 || p2){
-            p1 -> val += (p2 ? (p2 -> val) : 0) + carry;
-            carry = (p1 -> val) / 10;
-            p1 -> val %= 10;
+        while(l1 || l2){
+            l1 -> val += (l2 ? (l2 -> val) : 0) + carry;
+            carry = (l1 -> val) / 10;
+            l1 -> val %= 10;
             
-            prev = p1;
-            p1 = p1 -> next;
-            p2 = p2 ? p2 -> next: p2;
+            prev = l1;
+            l1 = l1 -> next;
+            if(l2) l2 = l2 -> next;
         }
-        if(carry)
-            prev -> next = new ListNode(carry);
-        return l1;
+        if(carry) prev -> next = new ListNode(carry);
+        return p1;
     }
 };
