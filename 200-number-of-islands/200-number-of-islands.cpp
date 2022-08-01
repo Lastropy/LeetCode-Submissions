a2a89315b42012f24da4430855591e69f82791f0
Shivam Shukla <19ucs040@lnmiit.ac.in>
2022-08-01 12:28:43 +0530

    Time: 58 ms (47.33%), Space: 12.2 MB (83.13%) - LeetHub

---
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void mark_by_dfs(vector<vector<char>> &grid, int x, int y){
+        if(x < 0 || x >= grid.size() || y < 0 || y >= grid[0].size() || grid[x][y] == '0' || grid[x][y] == '2')
+            return;
+        grid[x][y] = '2';
+        mark_by_dfs(grid, x+1 , y);
+        mark_by_dfs(grid, x-1 , y);
+        mark_by_dfs(grid, x , y+1);        
+        mark_by_dfs(grid, x , y-1);
+    }
+    int numIslands(vector<vector<char>>& grid) {
+        int ans = 0;
+        for(int i = 0; i < grid.size(); i++){
+            for(int j  = 0; j < grid[0].size(); j++){
+                if(grid[i][j] == '1'){
+                    ans++;
+                    // cout << ans << " th island found at (" <<i<<","<<j<<")\n"; 
+                    mark_by_dfs(grid,i, j);
+                }
+            }
+        }
+        return ans;
+    }
+};
