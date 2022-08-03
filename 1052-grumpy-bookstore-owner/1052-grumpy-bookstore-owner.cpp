2fe7b2269fafdcb334b454fdc561366dd2cce6ce
Shivam Shukla <19ucs040@lnmiit.ac.in>
2022-08-03 10:58:30 +0530

    Time: 49 ms (64.29%), Space: 31.8 MB (86.87%) - LeetHub

--- 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.cpp
+++ 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.cpp
@@ -1,29 +1,44 @@
 class Solution {
 public:
     int maxSatisfied(vector<int>& c, vector<int>& g, int k) {
-        vector<int> ps(g.size());
+        // vector<int> ps(g.size());
+        // int curr = 0;
+        // for(int i =0; i < g.size(); i++){
+        //     c[i] *= (g[i] == 0 ? 1: -1);
+        //     curr += c[i] > 0 ? c[i] : 0;
+        //     ps[i] = curr;
+        // }
+        // vector<int> ss(g.size());
+        // curr = 0;
+        // for(int i = g.size() - 1; i >= 0;i--){
+        //     curr += c[i] > 0 ? c[i] : 0;
+        //     ss[i] = curr;
+        // }
+        // int ans = INT_MIN;
+        // int sum = 0;
+        // for(int i = 0, j = 0; j < g.size(); j++){
+        //     sum += abs(c[j]);
+        //     if(j - i + 1 == k){
+        //         ans = max(ans, sum  + (i > 0 ? ps[i-1]: 0) + (j < g.size() - 1 ? ss[j+1] : 0));
+        //         sum -= abs(c[i]);
+        //         i++;
+        //     } 
+        // }
+        // return max(ans, sum);
+        
         int curr = 0;
-        for(int i =0; i < g.size(); i++){
-            c[i] *= (g[i] == 0 ? 1: -1);
-            curr += c[i] > 0 ? c[i] : 0;
-            ps[i] = curr;
-        }
-        vector<int> ss(g.size());
-        curr = 0;
-        for(int i = g.size() - 1; i >= 0;i--){
-            curr += c[i] > 0 ? c[i] : 0;
-            ss[i] = curr;
-        }
         int ans = INT_MIN;
-        int sum = 0;
-        for(int i = 0, j = 0; j < g.size(); j++){
-            sum += abs(c[j]);
-            if(j - i + 1 == k){
-                ans = max(ans, sum  + (i > 0 ? ps[i-1]: 0) + (j < g.size() - 1 ? ss[j+1] : 0));
-                sum -= abs(c[i]);
+        int add = 0;
+        for(int i = 0, j = 0; j < c.size(); j++){
+            curr += (!g[j]) * c[j];
+            add  += g[j] * c[j];
+            ans = max(add, ans);
+            if(j - i + 1 == k)
+            {
+                add -= g[i] * c[i];
                 i++;
-            } 
+            }
         }
-        return max(ans, sum);
+        return curr + ans;
     }
 };
