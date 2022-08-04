dca404e19f001cd5010973d7197cd5731587e893
Shivam Shukla <19ucs040@lnmiit.ac.in>
2022-08-04 15:23:34 +0530

    Time: 829 ms (12.30%), Space: 6.7 MB (64.93%) - LeetHub

--- 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.cpp
+++ 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.cpp
@@ -1,33 +1,16 @@
 class Solution {
 public:
      bool palin(string &s, int st, int e){
-        int i = st, j = e;
-        while(i < j)
-        {
-            if(s[i] != s[j])
-                return false;
-            i++; j--;
-        }
-        return true; 
+        if(st >= e) return true;
+        return (s[st] == s[e]) && palin(s, st + 1, e - 1);
+ 
     }
-    // bool palin(string &s, int st, int e, vector<vector<int>> &is){
-    //     if(is[st][e] != -1) return is[st][e];
-    //     int i = st, j = e;
-    //     while(i < j)
-    //     {
-    //         if(s[i] != s[j])
-    //             return is[st][e] = false;
-    //         i++; j--;
-    //     }
-    //     return is[st][e] = true; 
-    // }
-    // int f(string &s, int st, vector<int> &dp, vector<vector<int>> &is){
     int f(string &s, int st, vector<int> &dp){
 
         if(st >= s.size() - 1) return 0;
         if(dp[st] != -1) return dp[st];
         
-        int curr = -1;
+        int curr;
         int ans = INT_MAX;
         for(int i  = st; i < s.size();i++){
             if(palin(s, st, i)){
