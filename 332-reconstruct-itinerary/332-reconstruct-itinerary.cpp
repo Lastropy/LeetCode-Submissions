3095d335dd94277fbc233a8416bb49808891a6c2
Shivam Shukla <19ucs040@lnmiit.ac.in>
2022-08-05 15:04:46 +0530

    Time: 30 ms (74.35%), Space: 15.1 MB (46.68%) - LeetHub

--- 332-reconstruct-itinerary/332-reconstruct-itinerary.cpp
+++ 332-reconstruct-itinerary/332-reconstruct-itinerary.cpp
@@ -1,25 +1,19 @@
 class Solution {
 public:
+    void dfs(unordered_map<string, multiset<string>> &g, vector<string> &ans, string st){
+        while(!g[st].empty()){
+            string next = *g[st].begin();
+            g[st].erase(g[st].begin());
+            dfs(g, ans, next);
+        }
+        ans.push_back(st);
+    }
     vector<string> findItinerary(vector<vector<string>>& tickets) {
         unordered_map<string, multiset<string>> g;
         for(auto v: tickets)
             g[v[0]].insert(v[1]);
         vector<string> ans;
-        stack<string> st;
-        st.push("JFK");
-        
-        while(!st.empty()){
-            string src = st.top();
-            if(g[src].empty()){
-                ans.push_back(src);
-                st.pop();
-            }
-            else
-            {
-                st.push(*g[src].begin());
-                g[src].erase(g[src].begin());
-            }
-        }
+        dfs(g, ans, "JFK");
         reverse(ans.begin(), ans.end());
         return ans;
     }
