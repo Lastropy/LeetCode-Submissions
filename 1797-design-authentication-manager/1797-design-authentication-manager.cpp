d28fcb8759fd42f754c859aa13efb100577bb7f4
Shivam Shukla <19ucs040@lnmiit.ac.in>
2022-08-05 11:51:01 +0530

    Time: 116 ms (78.28%), Space: 31.6 MB (16.60%) - LeetHub

--- 1797-design-authentication-manager/1797-design-authentication-manager.cpp
+++ 1797-design-authentication-manager/1797-design-authentication-manager.cpp
@@ -2,7 +2,6 @@ class AuthenticationManager {
 public:
     int ttl;
     unordered_map<string, int> mp;
-    // map<int, int> times;
     multiset<int> times;
     AuthenticationManager(int timeToLive) {
         ttl = timeToLive;
@@ -10,41 +9,18 @@ public:
     
     void generate(string tokenId, int currentTime) {
         mp[tokenId] = currentTime + ttl;
-        // times[mp[tokenId]]++;
         times.insert(mp[tokenId]);
     }
     
     void renew(string tokenId, int currentTime) {
         if(mp.find(tokenId) == mp.end()) return;
         if(currentTime >= mp[tokenId]) return;
-        int old_exp = mp[tokenId];
-        int new_exp = currentTime + ttl;
-        mp[tokenId] =  new_exp;
-        // for(int i: times){
-        //     cout << i << ",";
-        // }
-        // cout << endl;
-        times.erase(times.find(old_exp));
-        times.insert(mp[tokenId]);
-        // for(int i: times){
-        //     cout << i << ",";
-        // }
-        // cout << endl;
-        // times[old_exp]--;
-        // times[new_exp]++;
+        times.erase(times.find(mp[tokenId]));
+        this -> generate(tokenId, currentTime);
     }
     
     int countUnexpiredTokens(int currentTime) {
-        int ans = 0;
-//        auto it = times.upper_bound(currentTime);
-       
-//        while(it != times.end()){
-//            ans += it -> second;
-//            advance(it, 1);
-//        }
-        auto it = times.upper_bound(currentTime);
-        ans = times.size() - distance(times.begin(), it);
-        return ans;
+        return times.size() - distance(times.begin(), times.upper_bound(currentTime));
     }
 };
 
