102ee86087eac8a533db04ad7b5350e76761c1ba
Shivam Shukla <19ucs040@lnmiit.ac.in>
2022-08-05 11:39:57 +0530

    Time: 196 ms (41.39%), Space: 31.4 MB (17.83%) - LeetHub

--- 1797-design-authentication-manager/1797-design-authentication-manager.cpp
+++ 1797-design-authentication-manager/1797-design-authentication-manager.cpp
@@ -2,27 +2,38 @@ class AuthenticationManager {
 public:
     int ttl;
     unordered_map<string, int> mp;
+    map<int, int> times;
     AuthenticationManager(int timeToLive) {
         ttl = timeToLive;
     }
     
     void generate(string tokenId, int currentTime) {
         mp[tokenId] = currentTime + ttl;
+        times[mp[tokenId]]++;
     }
     
     void renew(string tokenId, int currentTime) {
         if(mp.find(tokenId) == mp.end()) return;
         if(currentTime >= mp[tokenId]) return;
-        mp[tokenId] = currentTime + ttl;
+        int old_exp = mp[tokenId];
+        int new_exp = currentTime + ttl;
+        mp[tokenId] =  new_exp;
+        times[old_exp]--;
+        times[new_exp]++;
     }
     
     int countUnexpiredTokens(int currentTime) {
-        int ans = 0;
-        for(auto [k,v ] : mp){
-            if(v > currentTime)
-                ans++;
-        }
-        return ans;
+       auto it = times.upper_bound(currentTime);
+       int ans = 0;
+        // cout <<"At " <<currentTime << endl;
+       while(it != times.end()){
+           ans += it -> second;
+           // cout << it -> first << " -> " << it -> second << endl;
+           advance(it, 1);
+       }
+        cout << ans << endl;
+        // cout << "__________________" << endl;
+       return ans;
     }
 };
 
