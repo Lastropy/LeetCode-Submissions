d10c8da3e1279da5ecb83de5640218019f90a28c
Shivam Shukla <19ucs040@lnmiit.ac.in>
2022-08-06 12:07:48 +0530

    Time: 3 ms (40.36%), Space: 6 MB (58.91%) - LeetHub

--- 458-poor-pigs/458-poor-pigs.cpp
+++ 458-poor-pigs/458-poor-pigs.cpp
@@ -1,13 +1,7 @@
 class Solution {
 public:
     int poorPigs(int b, int mtd, int mtt) {
-        int nots = (mtt / mtd) + 1;
-        int ans = 0;
-        int res = 1;
-        while(res < b){
-            res *= nots;
-            ans++;
-        }
-        return ceil(log(b) / log(nots));
+        int num_of_tests = (mtt / mtd) + 1;
+        return ceil(log(b) / log(num_of_tests));
     }
 };
