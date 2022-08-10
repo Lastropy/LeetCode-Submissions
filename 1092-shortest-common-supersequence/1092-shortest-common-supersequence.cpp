ad095e90b8d8bb29f6308a08cbb59e32b5752216
Shivam Shukla <19ucs040@lnmiit.ac.in>
2022-08-10 19:21:08 +0530

    Time: 41 ms (35.58%), Space: 12.8 MB (53.29%) - LeetHub

--- 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
@@ -4,12 +4,10 @@ public:
         int n = s.size(), m = t.size();
         for(int i = 1; i < n+1; i++){
             for(int j = 1; j < m + 1; j++){
-                if(s[i-1] == t[j-1]){
+                if(s[i-1] == t[j-1])
                     dp[i][j] = 1 + dp[i-1][j-1];
-                }
-                else{
+                else
                     dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
-                }
             }
         }
     }
@@ -27,12 +25,9 @@ public:
             else if(dp[i-1][j] > dp[i][j-1]) ans += s[--i];
             else ans += t[--j];
         }
-        while(i)
-            ans += s[--i];
-        while(j)
-            ans += t[--j];
+        while(i)    ans += s[--i];
+        while(j)    ans += t[--j];
         reverse(ans.begin(), ans.end());
-      
         return ans;
     }
     string shortestCommonSupersequence(string s, string t) {
