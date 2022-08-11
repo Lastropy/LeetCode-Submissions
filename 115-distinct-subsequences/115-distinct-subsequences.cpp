f1ec2a97031b139624c5aea82589c5cba8435308
Shivam Shukla <19ucs040@lnmiit.ac.in>
2022-08-11 16:19:37 +0530

    Time: 60 ms (52.02%), Space: 6.4 MB (96.41%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.cpp
+++ 115-distinct-subsequences/115-distinct-subsequences.cpp
@@ -2,16 +2,20 @@ class Solution {
 public:
     int numDistinct(string s, string t) {
         int n = s.size(), m = t.size();
-        vector<vector<double>> dp(n+1, vector<double>(m+1, 0));
-        for(int i = 0; i <= n; i++)
-            dp[i][0] = 1;
+        vector<double> prev(m+1, 0), curr(m+1, 0);
+
+        prev[0] = 1;
+        curr[0] = 1;
+        
+        
         for(int i = 1; i <= n; i++){
             for(int j = 1; j <= m; j++){
-                dp[i][j] = dp[i-1][j];
+                curr[j] = prev[j];
                 if(s[i-1] == t[j-1])
-                    dp[i][j] += dp[i-1][j-1];                   
+                    curr[j] += prev[j-1];                   
             }
+            prev = curr;
         }
-        return (int)dp[n][m];
+        return (int)prev[m];
     }
 };
