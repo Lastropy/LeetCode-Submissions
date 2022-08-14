83d8e39871d66290923e764ce446772978b918c6
Shivam Shukla <19ucs040@lnmiit.ac.in>
2022-08-14 22:04:36 +0530

    Time: 582 ms (27.79%), Space: 10.6 MB (28.08%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
-
     int lengthOfLIS(vector<int>& a) {
         int n  = a.size();
-        vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
+        vector<int> curr(n+1, 0), next(n+1 , 0);
 
         for(int idx = n-1; idx >= 0; idx--){
 
             for(int prev = idx -1; prev >= -1; prev--){
 
-                int len = 0 + dp[idx+1][prev+1];    // not pick
+                int len = 0 + next[prev+1];    // not pick
 
                 if(prev == -1 || a[idx] > a[prev])
-                    len = max(len , 1 + dp[idx+1][idx+1]); // pick
+                    len = max(len , 1 + next[idx+1]); // pick
 
-                dp[idx][prev + 1] = len;
+                curr[prev + 1] = len;
 
             }
+            next = curr;
         }
 
-        return dp[0][0];
+        return curr[0];
     }
 };
