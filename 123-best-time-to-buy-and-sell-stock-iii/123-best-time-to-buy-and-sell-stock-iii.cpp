4b13667608ec9d98098860462d76b74730fbe660
Shivam Shukla <19ucs040@lnmiit.ac.in>
2022-08-14 16:53:56 +0530

    Time: 399 ms (48.13%), Space: 75.3 MB (86.19%) - LeetHub

--- 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -2,26 +2,26 @@ class Solution {
 public:
     int maxProfit(vector<int>& p) {
         int n = p.size();
-        vector<vector<vector<int>>> dp(n+1, vector<vector<int>>(2, vector<int>(3, 0)));
-        
+        vector<vector<int>> curr(2, vector<int>(3, 0)), next(2, vector<int>(3, 0));
         for(int idx = n-1; idx >= 0; idx--){
             for(int buy = 0; buy <= 1; buy++){
                 for(int lim = 0; lim <= 2; lim++){
                     if(lim == 0) 
-                        dp[idx][buy][lim] = 0;
+                        curr[buy][lim] = 0;
                     else if(buy){
-                        int b = -p[idx] + dp[idx+ 1][0][lim];
-                        int nb = 0 + dp[idx + 1][1][lim];
-                        dp[idx][buy][lim] = max(b, nb);   
+                        int b = -p[idx] + next[0][lim];
+                        int nb = 0 + next[1][lim];
+                        curr[buy][lim] = max(b, nb);   
                     }
                     else{
-                        int s = p[idx] + dp[idx+ 1][1][lim-1];
-                        int ns = 0 + dp[idx + 1][0][lim];
-                        dp[idx][buy][lim] = max(s, ns);  
+                        int s = p[idx] + next[1][lim-1];
+                        int ns = 0 + next[0][lim];
+                        curr[buy][lim] = max(s, ns);  
                     }
                 }
             }
+            next = curr;
         }
-        return dp[0][1][2];
+        return curr[1][2];
     }
 };
