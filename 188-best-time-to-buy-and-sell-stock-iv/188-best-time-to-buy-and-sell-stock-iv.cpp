4cfda007a32d9fdb8c3281af0c30623f97dbcb71
Shivam Shukla <19ucs040@lnmiit.ac.in>
2022-08-14 17:08:43 +0530

    Time: 21 ms (46.55%), Space: 13 MB (35.39%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,25 +1,35 @@
 class Solution {
 public:
-    int f(vector<int> &p, int idx, bool buy, int lim, vector<vector<vector<int>>> &dp){
+    int maxProfit(int k , vector<int>& p) {
         int n = p.size();
-        if(lim == 0 || idx == n) return 0; 
-        if(dp[idx][buy][lim] != -1) return dp[idx][buy][lim];
-        int ans = 0;
-        if(buy){
-            int b = -p[idx] + f(p, idx + 1, 0, lim, dp);
-            int nb = 0 + f(p, idx + 1, 1, lim, dp);
-            ans = max(b, nb);
+
+        vector<vector<vector<int>>> dp(n+1, vector<vector<int>>(2, vector<int>(k+1, 0)));
+        
+        for(int idx = n-1; idx >= 0; idx--){
+
+            for(int buy = 0; buy <= 1; buy++){
+
+                for(int lim = 0; lim <= k; lim++){
+
+                    if(lim == 0) // limit zero means can't buy anything now
+                        dp[idx][buy][lim] = 0;
+                    else if(buy){
+                        int b = -p[idx] + dp[idx+ 1][0][lim];
+
+                        int nb = 0 + dp[idx + 1][1][lim];
+
+                        dp[idx][buy][lim] = max(b, nb);   
+                    }
+                    else{
+                        int s = p[idx] + dp[idx+ 1][1][lim-1];
+
+                        int ns = 0 + dp[idx + 1][0][lim];
+
+                        dp[idx][buy][lim] = max(s, ns);  
+                    }
+                }
+            }
         }
-        else{
-            int s = p[idx] + f(p, idx+1, 1, lim-1, dp);
-            int ns = 0 + f(p, idx+1, 0, lim, dp);
-            ans = max(s, ns);
-        }
-        return dp[idx][buy][lim] = ans;
-    }
-    int maxProfit(int k, vector<int>& p) {
-        int n = p.size();
-        vector<vector<vector<int>>> dp(n, vector<vector<int>>(2, vector<int>(k+1, -1)));
-        return f(p, 0, 1, k, dp);
+        return dp[0][1][k];
     }
 };
