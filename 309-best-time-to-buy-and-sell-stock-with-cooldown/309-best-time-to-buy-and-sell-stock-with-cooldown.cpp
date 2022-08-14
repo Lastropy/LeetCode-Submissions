829d4d8176084acc47d5b46a403444a268fcfafb
Shivam Shukla <19ucs040@lnmiit.ac.in>
2022-08-14 17:36:44 +0530

    Time: 9 ms (43.91%), Space: 12.8 MB (7.60%) - LeetHub

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,30 +1,27 @@
 class Solution {
 public:
-    int f(vector<int> &p, int idx, bool buy, bool cool_down, vector<vector<vector<int>>> &dp){
-        int n = p.size();
-        if(idx >= n) return 0; 
-
-        if(dp[idx][buy][cool_down] != -1) return dp[idx][buy][cool_down];
-        int ans = 0;
-        
-        if(buy){
-            int b = -p[idx] + f(p, idx + 1, 0, 0, dp); 
-            int nb = 0 + f(p, idx + 1, 1, 0, dp);
-
-            if(cool_down) ans = nb;
-            else ans = max(b, nb);
-        }
-        else{
-            int s = p[idx] + f(p, idx+1, 1, 1, dp);
-            int ns = 0 + f(p, idx+1, 0, 0, dp);
-            ans = max(s, ns);
-        }
-        return dp[idx][buy][cool_down] = ans;
-    }
     int maxProfit(vector<int>& p) {
         bool cool_down = 0;
         int n = p.size();
-        vector<vector<vector<int>>> dp(n, vector<vector<int>>(2, vector<int>(2, -1)));
-        return f(p, 0, 1, 0, dp);
+        vector<vector<vector<int>>> dp(n+1, vector<vector<int>>(2, vector<int>(2, 0)));
+        for(int idx = n-1; idx >= 0; idx--){
+            for(int buy = 0; buy <= 1; buy++){
+                for(int cool_down = 0; cool_down <= 1; cool_down++){
+                    if(buy){
+                        int b = -p[idx] + dp[idx + 1][0][0]; 
+                        int nb = 0 + dp[idx + 1][1][0];
+
+                        if(cool_down) dp[idx][buy][cool_down] = nb;
+                        else dp[idx][buy][cool_down] = max(b, nb);
+                    }
+                    else{
+                        int s = p[idx] + dp[idx+1][1][1];
+                        int ns = 0 + dp[idx+1][0][0];
+                        dp[idx][buy][cool_down] = max(s, ns);
+                    }
+                }
+            }
+        }
+        return dp[0][1][0];
     }
 };
