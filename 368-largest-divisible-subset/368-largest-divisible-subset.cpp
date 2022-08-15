9053e20687582463aed9069ac33b4962ffce4911
Shivam Shukla <19ucs040@lnmiit.ac.in>
2022-08-15 14:18:40 +0530

    Time: 57 ms (87.65%), Space: 9 MB (47.35%) - LeetHub

--- 368-largest-divisible-subset/368-largest-divisible-subset.cpp
+++ 368-largest-divisible-subset/368-largest-divisible-subset.cpp
@@ -7,37 +7,36 @@ public:
         vector<int> dp(n, 1);
 
         vector<int> hash(n);
+
+        int ans = INT_MIN;
+        int curr_hash = 0;
+
         for(int i = 0; i < n; i++) hash[i] = i;
 
         for(int i = 0; i < n; i++){
             for(int prev = 0; prev < i; prev++){
                 if(a[i] % a[prev] == 0){
-                    if(dp[prev] + 1 > dp[i]){
-                        dp[i] = 1 + dp[prev];
-                        hash[i] = prev;
-                    }
+                    if(dp[prev] + 1 > dp[i])  hash[i] = prev;
+                    dp[i] = max(dp[i] , dp[prev] + 1);
                 }
             }
+
+            if(dp[i] > ans) curr_hash = i;
+            ans = max(ans, dp[i]);
         }
-        
-        int ans = INT_MIN;
-        int h = 0;
-        for(int i = 0; i < dp.size(); i++){
-            if(dp[i] > ans){
-                ans = dp[i];
-                h = i;
-            }
-        }
+    
         vector<int> res;
-        int old = -1;
-        while(true){
-            if(old == h) break;
-            res.push_back(a[h]);
-            old = h;
-            h = hash[h];
+        int old_hash = -1;
+
+        while(old_hash != curr_hash){
+            res.push_back(a[curr_hash]);
+
+            old_hash = curr_hash;
+            curr_hash = hash[curr_hash];
         }
         
         reverse(begin(res), end(res));
+
         return res;
     }
 };
