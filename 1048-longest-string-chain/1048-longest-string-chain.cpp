ebe1f271f09dce04f0674b990a8c6a9127b9cc7b
Shivam Shukla <19ucs040@lnmiit.ac.in>
2022-08-16 18:44:36 +0530

    Time: 184 ms (41.26%), Space: 29.5 MB (19.38%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.cpp
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     bool check(string &p, string &n){
-        // cout << " comparing "<< p << " and " << n ;
         if(n.size() - p.size() != 1) return false;
         int p1 = 0, p2 = 0;
         while(p1 < p.size() && p2 < n.size()){
@@ -17,27 +16,16 @@ public:
         sort(begin(w), end(w), [&](string a, string b){
           return a.size() == b.size() ? a < b: a.size() < b.size();  
         });
-        int n = w.size();
 
-        vector<int> dp(n, 1);
-
-        
-        for(int idx = 0; idx < n; idx++)
-            for(int prev = 0; prev < idx; prev++){
-                bool fin = check(w[prev], w[idx]);
-                // cout << " -> " << fin << endl;
-                if(fin)
-                    dp[idx]  = max(dp[idx], dp[prev]+1);
-            }
-        
+        vector<int> dp(w.size(), 1);
         int ans = INT_MIN;
-        int cnt = 0;
-        for(int i : dp){
-            // cout << w[cnt++] << " " << i << endl;
-            ans = max(ans , i);
+
+        for(int idx = 0; idx < w.size(); idx++){
+            for(int prev = 0; prev < idx; prev++)
+                if(check(w[prev], w[idx]))
+                    dp[idx]  = max(dp[idx], dp[prev]+1);
+            ans = max(ans, dp[idx]);
         }
         return ans;
-      
-        
     }
 };
