9ebf54d0892ee74745bb01a468ce2afb57730789
Shivam Shukla <19ucs040@lnmiit.ac.in>
2022-08-17 09:33:45 +0530

    Time: 7 ms (46.48%), Space: 8.6 MB (30.73%) - LeetHub

--- 804-unique-morse-code-words/804-unique-morse-code-words.cpp
+++ 804-unique-morse-code-words/804-unique-morse-code-words.cpp
@@ -2,13 +2,19 @@ class Solution {
 public:
     int uniqueMorseRepresentations(vector<string>& words) {
         vector<string> morse({".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."});
+
         set<string> ans;
+
         for(string s: words){
+
             string res = "";
+
             for(char c: s)
                 res += morse[c-'a'];
+
             ans.insert(res);
         }
+
         return ans.size();
     }
 };
