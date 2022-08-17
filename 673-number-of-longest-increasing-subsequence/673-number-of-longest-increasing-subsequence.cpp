f6aded7ee1517b35366486dfbe9f757a4772cfed
Shivam Shukla <19ucs040@lnmiit.ac.in>
2022-08-17 16:18:33 +0530

    Time: 196 ms (75.76%), Space: 13.3 MB (52.05%) - LeetHub

--- 673-number-of-longest-increasing-subsequence/673-number-of-longest-increasing-subsequence.cpp
+++ 673-number-of-longest-increasing-subsequence/673-number-of-longest-increasing-subsequence.cpp
@@ -7,24 +7,20 @@ public:
         
         for(int idx = 0; idx < n; idx++){
             for(int prev = 0; prev < idx; prev++){
-                if(a[prev] < a[idx]){
-                    if(dp[prev] + 1 > dp[idx]){
-                        dp[idx] = dp[prev]+ 1;
-                        cnt[idx] = cnt[prev];
-                    }
-                    else if(dp[prev] + 1 == dp[idx]){
-                        cnt[idx] += cnt[prev];
-                    }
+                if(a[prev] < a[idx] && dp[prev] + 1 >= dp[idx]){
+                    if(dp[prev] + 1 > dp[idx]) 
+                        cnt[idx] = 0;
+                    dp[idx] = dp[prev]+ 1;
+                    cnt[idx] += cnt[prev];
                 }
             }
         }
         int ans1 = INT_MIN, ans2 = 0;
         for(int i = 0; i < n;i++){
-            if(dp[i] > ans1){
+            if(dp[i] >= ans1){
+                if(dp[i] > ans1) 
+                    ans2 = 0;
                 ans1 = dp[i];
-                ans2 = cnt[i];
-            }
-            else if(dp[i] == ans1){
                 ans2 += cnt[i];
             }
         }
