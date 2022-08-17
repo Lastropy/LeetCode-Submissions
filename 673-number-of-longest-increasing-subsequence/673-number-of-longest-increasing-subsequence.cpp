73a7f35a5a74a325a3f3a67cc885d6c7c027c27a
Shivam Shukla <19ucs040@lnmiit.ac.in>
2022-08-17 16:51:30 +0530

    Time: 313 ms (25.43%), Space: 13.2 MB (52.05%) - LeetHub

--- 673-number-of-longest-increasing-subsequence/673-number-of-longest-increasing-subsequence.cpp
+++ 673-number-of-longest-increasing-subsequence/673-number-of-longest-increasing-subsequence.cpp
@@ -7,23 +7,32 @@ public:
         
         for(int idx = 0; idx < n; idx++){
             for(int prev = 0; prev < idx; prev++){
+
                 if(a[prev] < a[idx] && dp[prev] + 1 >= dp[idx]){
+					// if greater length LIS found, our current count of max length LIS is irrelevant now. Hence, reset to zero.
                     if(dp[prev] + 1 > dp[idx]) 
                         cnt[idx] = 0;
+
                     dp[idx] = dp[prev]+ 1;
                     cnt[idx] += cnt[prev];
+
                 }
             }
         }
-        int ans1 = INT_MIN, ans2 = 0;
+
+        int max_len = INT_MIN, count = 0;
+
         for(int i = 0; i < n;i++){
-            if(dp[i] >= ans1){
-                if(dp[i] > ans1) 
-                    ans2 = 0;
-                ans1 = dp[i];
-                ans2 += cnt[i];
+
+            if(dp[i] >= max_len){
+				// Note the max length LIS. If greater Length LIS found, reset current count to zero.
+                if(dp[i] > max_len) count = 0;
+
+                max_len = dp[i];
+                count += cnt[i];
+
             }
         }
-        return ans2;
+        return count;
     }
 };
