6bbba456da8b8c589f6e727b9b8faef80e22dded
Shivam Shukla <19ucs040@lnmiit.ac.in>
2022-08-18 08:30:59 +0530

    Time: 296 ms (67.00%), Space: 77.8 MB (86.06%) - LeetHub

--- 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.cpp
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.cpp
@@ -1,19 +1,41 @@
 class Solution {
 public:
-    int minSetSize(vector<int>& arr) {
-        unordered_map<int, int> mp;
-        vector<int> freq;
+         int minSetSize(vector<int>& arr) {
+            int n = arr.size(), tar = arr.size() / 2;
+            unordered_map<int, int> mp;
+            vector<int> freq(n+1, 0);
 
-        for(int i : arr) mp[i]++;
-        for(auto [k,v]: mp) freq.push_back(v);
+            for(int i : arr) mp[i]++;
+            for(auto [k,v]: mp) freq[v]++;
+
+            int ptr = freq.size() - 1 , ans = 0 ;
+
+            while(n > tar){
+                while(n > tar && freq[ptr]){
+                    ans++;
+                    n -= ptr;
+                    freq[ptr]--;
+                }
+                ptr--;
+            }       
+               
+
+            return ans;
+        }
+//     int minSetSize(vector<int>& arr) {
+//         unordered_map<int, int> mp;
+//         vector<int> freq;
+
+//         for(int i : arr) mp[i]++;
+//         for(auto [k,v]: mp) freq.push_back(v);
         
-        sort(freq.begin(), freq.end());
+//         sort(freq.begin(), freq.end());
         
-        int n = arr.size(), tar = arr.size() / 2, ptr = freq.size() - 1;
+//         int n = arr.size(), tar = arr.size() / 2, ptr = freq.size() - 1;
 
-        while(n > tar)
-            n -= freq[ptr--];
+//         while(n > tar)
+//             n -= freq[ptr--];
 
-        return freq.size() - 1 - ptr;
-    }
+//         return freq.size() - 1 - ptr;
+//     }
 };
