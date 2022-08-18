22c364035791ee72b9ce02b3e1f884590d46893e
Shivam Shukla <19ucs040@lnmiit.ac.in>
2022-08-18 08:20:11 +0530

    Time: 276 ms (74.02%), Space: 78.4 MB (63.73%) - LeetHub

--- 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.cpp
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.cpp
@@ -2,37 +2,21 @@ class Solution {
 public:
     int minSetSize(vector<int>& arr) {
         unordered_map<int, int> mp;
-        for(int i : arr)
-            mp[i]++;
-        
         vector<int> freq;
-        for(auto [k,v]: mp)
-            freq.push_back(v);
+
+        for(int i : arr) mp[i]++;
+        for(auto [k,v]: mp) freq.push_back(v);
         
         sort(freq.begin(), freq.end());
         
         int n = arr.size(), tar = arr.size() / 2, ans = 0;
 
-        for(auto it = freq.rbegin(); it != freq.rend() && n > tar; it++){
-            int val = *it;
-            n -= val;
+        while(n > tar){
+            n -= freq.back();
+            freq.pop_back();
             ans++;
         }
 
-    
-
-        // priority_queue<int, vector<int>> pq;
-        // for(auto [k,v] : mp)
-        //     pq.push(v);
-        
-
-        
-//         while(n > tar){
-//             int tmp = pq.top();
-//             pq.pop();
-//             n-= tmp;
-//             ans++;
-//         }
         return ans;
     }
 };
