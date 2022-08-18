10e80baa7f55e7b5f81b7ed86dd5ab6f14e13fa0
Shivam Shukla <19ucs040@lnmiit.ac.in>
2022-08-18 08:16:57 +0530

    Time: 267 ms (76.66%), Space: 78.3 MB (79.94%) - LeetHub

--- 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.cpp
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.cpp
@@ -1,21 +1,38 @@
 class Solution {
 public:
     int minSetSize(vector<int>& arr) {
-        map<int, int> mp;
+        unordered_map<int, int> mp;
         for(int i : arr)
             mp[i]++;
-        int n = arr.size();
-        int tar = arr.size() / 2;
-        priority_queue<int, vector<int>> pq;
-        for(auto [k,v] : mp)
-            pq.push(v);
-        int ans = 0;
-        while(n > tar){
-            int tmp = pq.top();
-            pq.pop();
-            n-= tmp;
+        
+        vector<int> freq;
+        for(auto [k,v]: mp)
+            freq.push_back(v);
+        
+        sort(freq.begin(), freq.end());
+        
+        int n = arr.size(), tar = arr.size() / 2, ans = 0;
+
+        for(auto it = freq.rbegin(); it != freq.rend() && n > tar; it++){
+            int val = *it;
+            n -= val;
             ans++;
         }
+
+    
+
+        // priority_queue<int, vector<int>> pq;
+        // for(auto [k,v] : mp)
+        //     pq.push(v);
+        
+
+        
+//         while(n > tar){
+//             int tmp = pq.top();
+//             pq.pop();
+//             n-= tmp;
+//             ans++;
+//         }
         return ans;
     }
 };
