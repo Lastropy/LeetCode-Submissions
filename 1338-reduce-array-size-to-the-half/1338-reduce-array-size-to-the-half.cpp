c0772e9301def6fff7e289de1be49c8f46ea73b6
Shivam Shukla <19ucs040@lnmiit.ac.in>
2022-08-18 08:45:59 +0530

    Time: 168 ms (98.52%), Space: 75.3 MB (89.65%) - LeetHub

--- 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.cpp
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.cpp
@@ -1,27 +1,46 @@
 class Solution {
 public:
-         int minSetSize(vector<int>& arr) {
+        int minSetSize(vector<int>& arr) {
             int n = arr.size(), tar = arr.size() / 2;
             unordered_map<int, int> mp;
-            vector<int> freq(n+1, 0);
+            map<int, int> freq;
+            // vector<int> freq(n+1, 0);
 
             for(int i : arr) mp[i]++;
             for(auto [k,v]: mp) freq[v]++;
 
-            int ptr = freq.size() - 1 , ans = 0 ;
-
-            while(n > tar){
-                while(n > tar && freq[ptr]){
+            int ans = 0;
+            for(auto it = freq.rbegin(); it != freq.rend() && n > tar; it++){
+                int val = it -> first;
+                int cnt = it -> second;
+                while(n > tar && cnt){
+                    n -= val;
+                    cnt--;
                     ans++;
-                    n -= ptr;
-                    freq[ptr]--;
                 }
-                ptr--;
             }       
-               
-
             return ans;
         }
+//          int minSetSize(vector<int>& arr) {
+//             int n = arr.size(), tar = arr.size() / 2;
+//             unordered_map<int, int> mp;
+//             vector<int> freq(n+1, 0);
+
+//             for(int i : arr) mp[i]++;
+//             for(auto [k,v]: mp) freq[v]++;
+
+//             int ptr = freq.size() - 1 , ans = 0 ;
+
+//             while(n > tar){
+//                 while(n > tar && freq[ptr]){
+//                     ans++;
+//                     n -= ptr;
+//                     freq[ptr]--;
+//                 }
+//                 ptr--;
+//             }       
+//             return ans;
+//         }
 //     int minSetSize(vector<int>& arr) {
 //         unordered_map<int, int> mp;
 //         vector<int> freq;
