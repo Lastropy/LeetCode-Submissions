baacb9347f7a7ddc6907a81df1d30de5ac1c632c
Shivam Shukla <19ucs040@lnmiit.ac.in>
2022-08-21 17:45:48 +0530

    Time: 572 ms (63.38%), Space: 10.4 MB (52.38%) - LeetHub

--- 312-burst-balloons/312-burst-balloons.cpp
+++ 312-burst-balloons/312-burst-balloons.cpp
@@ -2,15 +2,19 @@ class Solution {
 public:
     int f(vector<int> &a, int i, int j, vector<vector<int>> &dp){
         if(i > j) return 0;
+
         if(dp[i][j] != -1)
             return dp[i][j];
+
         int ans = INT_MIN;
+
         for(int k = i; k <= j; k++){
-            int tmp = f(a, i, k-1, dp) + f(a, k+1, j, dp) + (a[i-1] * a[k] * a[j+1]);
-            ans = max(ans, tmp);
+            int tmp = f(a, i, k-1, dp) + f(a, k+1, j, dp);
+            ans = max(ans, tmp + (a[i-1] * a[k] * a[j+1]));
         }
         return dp[i][j] = ans;
     }
+    
     int maxCoins(vector<int>& nums) {
         int n = nums.size();
         nums.insert(nums.begin() , 1);
