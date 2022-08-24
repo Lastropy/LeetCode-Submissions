7c16d2b8caec7a2d8370aa37f6d253fade5cd6c0
Shivam Shukla <19ucs040@lnmiit.ac.in>
2022-08-24 08:17:27 +0530

    Time: 20 ms (88.73%), Space: 6.1 MB (7.72%) - LeetHub

--- 326-power-of-three/326-power-of-three.cpp
+++ 326-power-of-three/326-power-of-three.cpp
@@ -3,9 +3,13 @@ public:
     bool isPowerOfThree(int n) {
         if(n == 0) return false;
         if(n == 1) return true;
-        long l = 1;
-        while(l < n)
-            l *= 3;
-        return l == n;
+        // long l = 1;
+        // while(l < n)
+        //     l *= 3;
+        // return l == n;
+        
+        double div = log10(n) / log10(3);
+        return (fmod(div, 1)) == 0.00000;
+        
     }
 };
