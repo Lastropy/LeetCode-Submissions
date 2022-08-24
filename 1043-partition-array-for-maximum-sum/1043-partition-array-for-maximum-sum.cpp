cc93a615ee280655b65aa1d0e5d6eb547b78556a
Shivam Shukla <19ucs040@lnmiit.ac.in>
2022-08-24 09:58:02 +0530

    Time: 17 ms (85.99%), Space: 8.4 MB (92.07%) - LeetHub

--- 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
@@ -18,8 +18,27 @@ public:
 
         return dp[idx] = ans;
     }
-    int maxSumAfterPartitioning(vector<int>& arr, int k) {
-        vector<int> dp(arr.size() + 1, -1);
-        return f(arr, 0, k, dp);
+    int maxSumAfterPartitioning(vector<int>& a, int k) {
+        // vector<int> dp(a.size() + 1, -1);
+        // return f(a, 0, k, dp);
+
+        int n = a.size();
+        vector<int> dp(n + 1, 0);
+
+        for(int idx = n - 1; idx >= 0; idx--){
+            int mx = INT_MIN;
+            int ans = INT_MIN;
+
+
+            for(int j = idx; j < min(n, idx + k); j++){
+                mx = max(mx, a[j]);
+                int sum = (mx * (j - idx + 1)) + dp[j+1];
+                ans = max(ans, sum);
+            }
+            
+            dp[idx] = ans;
+        }
+        
+        return dp[0];
     }
 };
