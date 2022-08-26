a356e1f30427ed92e44ac0cfcd4fc02ade055e25
Shivam Shukla <19ucs040@lnmiit.ac.in>
2022-08-26 12:28:56 +0530

    Time: 4 ms (61.58%), Space: 6 MB (83.90%) - LeetHub

--- 869-reordered-power-of-2/869-reordered-power-of-2.cpp
+++ 869-reordered-power-of-2/869-reordered-power-of-2.cpp
@@ -15,17 +15,13 @@ public:
         int curr= 1;
 
         string s = to_string(curr);        
-        // cout << s << endl;
+
         while(len >= s.size() && curr <= 1e9){
             if(len == s.size()){
-                // cout << s << endl;
                 vector<int> cpy(f.begin(), f.end());
 
                 for(char c: s)
                     cpy[c-'0']--;
-                
-                // for(int i= 0; i <= 9; i++)
-                    // cout << i << " - " << cpy[i] << endl;
 
                 if(cpy == target)
                     return true; 
