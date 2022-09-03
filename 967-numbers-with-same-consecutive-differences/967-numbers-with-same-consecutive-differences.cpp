0b168a3cf8ca85f31018ab23cad1acbbd17a2ef5
Shivam Shukla <19ucs040@lnmiit.ac.in>
2022-09-03 07:51:43 +0530

    Time: 7 ms (38.82%), Space: 8.7 MB (37.95%) - LeetHub

--- 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.cpp
+++ 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.cpp
@@ -1,29 +1,43 @@
 class Solution {
 public:
-    void f(int n, int k, int prev, vector<int> &ans, string curr= ""){
+//     void f(int n, int k, int prev, vector<int> &ans, string curr= ""){
+//         if(n == 0){
+//             ans.push_back(stoi(curr));
+//             return;
+//         }
+        
+        
+//         if(prev == -1){
+//             for(int i = 1; i <= 9; i++){
+//                 f(n-1,k, i, ans, curr + to_string(i));       
+//             }
+//         }
+//         else{
+//             int pos1 = prev + k;
+//             int pos2 = prev - k;
+//             if(pos1 >= 0 && pos1 <= 9) 
+//                 f(n-1, k , pos1, ans, curr + to_string(pos1));
+//             if(pos1 != pos2 && pos2 >= 0 && pos2 <= 9) 
+//                 f(n-1, k , pos2, ans, curr + to_string(pos2));
+//         }
+//     }
+    void f(int n, int k, int res, vector<int> &ans){
         if(n == 0){
-            ans.push_back(stoi(curr));
+            ans.push_back(res);
             return;
         }
-        
-        
-        if(prev == -1){
-            for(int i = 1; i <= 9; i++){
-                f(n-1,k, i, ans, curr + to_string(i));       
-            }
-        }
-        else{
-            int pos1 = prev + k;
-            int pos2 = prev - k;
-            if(pos1 >= 0 && pos1 <= 9) 
-                f(n-1, k , pos1, ans, curr + to_string(pos1));
-            if(pos1 != pos2 && pos2 >= 0 && pos2 <= 9) 
-                f(n-1, k , pos2, ans, curr + to_string(pos2));
-        }
+
+        int pos1 = (res % 10) + k;
+        int pos2 = (res % 10) - k;
+        if(pos1 >= 0 && pos1 <= 9) 
+            f(n-1, k , res*10 + pos1, ans);
+        if(pos1 != pos2 && pos2 >= 0 && pos2 <= 9) 
+            f(n-1, k , res*10 + pos2, ans);
     }
     vector<int> numsSameConsecDiff(int n, int k) {
         vector<int> ans;
-        f(n, k, -1, ans);
+        for(int i = 1; i <= 9; i++)
+            f(n - 1, k, i, ans);
         return ans;
     }
 };
