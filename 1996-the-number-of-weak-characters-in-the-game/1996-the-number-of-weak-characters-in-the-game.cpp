757ffeba10bee83e790c9b35b215624088fe7790
Shivam Shukla <19ucs040@lnmiit.ac.in>
2022-09-09 08:23:17 +0530

    Time: 1407 ms (20.59%), Space: 125.1 MB (91.78%) - LeetHub

--- 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.cpp
+++ 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
-    static bool comp(vector<int> &a,vector<int> &b){
-        if(a[0]==b[0]){
-            return a[1]>b[1];
-        }
-        return a[0]<b[0];
-    }
-    int numberOfWeakCharacters(vector<vector<int>>& prop) {
-        sort(prop.begin(),prop.end(),comp);
-        int maxTillNow = INT_MIN;
-        int ans=0;
-        for(int i=prop.size()-1;i>=0;i--){
-            if(prop[i][1]<maxTillNow){
+    int numberOfWeakCharacters(vector<vector<int>>& properties) {
+        sort(properties.begin(), properties.end(), [&](vector<int> &a, vector<int> &b){
+           return a[0] == b[0] ? a[1] > b[1] : a[0] < b[0]; 
+        });
+        
+        
+        int ans = 0;
+        int mx = INT_MIN;
+        for(int i = properties.size() - 1; i >= 0; i--){
+            if(properties[i][1] < mx)
                 ans++;
-            }
-            maxTillNow = max(maxTillNow,prop[i][1]);
+            mx = max(mx, properties[i][1]);
         }
+        
         return ans;
     }
 };
