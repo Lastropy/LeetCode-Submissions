8c1d8700ff7a1dedd7ee8af218788cd6c0cedc7a
Shivam Shukla <19ucs040@lnmiit.ac.in>
2022-09-11 11:15:27 +0530

    Time: 807 ms (50.00%), Space: 145.8 MB (25.00%) - LeetHub

--- 2406-divide-intervals-into-minimum-number-of-groups/2406-divide-intervals-into-minimum-number-of-groups.cpp
+++ 2406-divide-intervals-into-minimum-number-of-groups/2406-divide-intervals-into-minimum-number-of-groups.cpp
@@ -4,9 +4,9 @@ public:
 
         vector<vector<int>> log;
         
-        for(int i = 0; i < intervals.size(); i++){
-            log.push_back({intervals[i][0], 1});
-            log.push_back({intervals[i][1] + 1, -1});
+        for(auto &v: intervals){
+            log.push_back({v[0], 1});
+            log.push_back({v[1] + 1, -1});
         }
         
         sort(log.begin(), log.end());
