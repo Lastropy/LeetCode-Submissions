33ee7159057fe3bb1d91ebc314805999359f642c
Shivam Shukla <19ucs040@lnmiit.ac.in>
2022-09-12 16:48:02 +0530

    Time: 21 ms (7.28%), Space: 10.5 MB (66.86%) - LeetHub

--- 948-bag-of-tokens/948-bag-of-tokens.cpp
+++ 948-bag-of-tokens/948-bag-of-tokens.cpp
@@ -3,22 +3,23 @@ public:
     int bagOfTokensScore(vector<int>& tokens, int power) {
         sort(tokens.begin(), tokens.end());
         
-        int curr_score = 0, max_score = 0;
+        int curr_score = 0;
         int s = 0, e = tokens.size() - 1;
-        while( s <= e){
+
+        while( s <= e ){
+
             if(tokens[s] <= power){
                 power -= tokens[s++];
                 curr_score++;
             }
-            else if(curr_score >= 1){
+            
+            else if(curr_score > 0 && s < e){
                 power += tokens[e--];
                 curr_score--;
             }
-            else{
-                s++;
-            }
-            max_score = max(max_score, curr_score);
+            
+            else s++;
         }
-        return max_score;
+        return curr_score;
     }
 };
