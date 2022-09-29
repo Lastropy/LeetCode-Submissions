4b57f13cd2986af5d12ef8bda6c95bfe13626b04
Shivam Shukla <19ucs040@lnmiit.ac.in>
2022-09-29 10:56:42 +0530

    Time: 228 ms (5.93%), Space: 34.1 MB (23.63%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.cpp
+++ 658-find-k-closest-elements/658-find-k-closest-elements.cpp
@@ -4,21 +4,21 @@ public:
         int n = arr.size();
         if(n == 1) return vector<int>({arr[0]});
         if(n == k) return arr;
-        // if(x < arr[0])
-        //     return vector<int>(arr.begin(), arr.begin()+ k);
-        // else if(x > arr[n -1])
-        //     return vector<int>(arr.begin() + n - k, arr.end());
+
         priority_queue<pair<int,int>> max_heap;
         vector<int> ans;
+
         for(int i = 0; i < n; i++){
             max_heap.push({abs(arr[i]-x),arr[i]});
             if(max_heap.size() > k)
                 max_heap.pop();
         }
+
         while(!max_heap.empty()){
             ans.push_back(max_heap.top().second);
             max_heap.pop();
         }
+
         sort(ans.begin(),ans.end());
         return ans;
     }
