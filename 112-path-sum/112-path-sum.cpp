bfda105ed3e8577306f8e7b1e6aee26421180397
Shivam Shukla <19ucs040@lnmiit.ac.in>
2022-10-04 09:52:54 +0530

    Time: 25 ms (25.89%), Space: 21.2 MB (74.14%) - LeetHub

--- 112-path-sum/112-path-sum.cpp
+++ 112-path-sum/112-path-sum.cpp
@@ -11,15 +11,12 @@
  */
 class Solution {
 public:
-    bool f(TreeNode *root, int tar){
+    bool hasPathSum(TreeNode* root, int tar) {
         if(!root) return false;
+
         tar -= root -> val;
         if(!(root -> left) && !(root -> right)) return tar == 0;
-        
-        return f(root -> left, tar) || f (root -> right, tar);
-    }
-    bool hasPathSum(TreeNode* root, int targetSum) {
-        // if(!root) return false;
-        return f(root, targetSum);
+
+        return hasPathSum(root -> left, tar) || hasPathSum(root -> right, tar);
     }
 };
