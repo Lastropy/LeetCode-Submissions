89933931ec807d6379c180e539d3da5af7451e5d
Shivam Shukla <19ucs040@lnmiit.ac.in>
2022-10-30 12:35:13 +0530

    Time: 407 ms (36.08%), Space: 50.8 MB (35.76%) - LeetHub

--- 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.cpp
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.cpp
@@ -2,35 +2,40 @@ class Solution {
 public:
     int shortestPath(vector<vector<int>>& grid, int k) {
         int m = grid.size(), n = grid[0].size();
-        vector<vector<int>> vis(m, vector<int>(n , -1));
-        
+        vector<vector<int>> vis(m, vector<int>(n, -1));
         queue<vector<int>> q;
         
-        for(q.push({0, 0 , k, 0}); !q.empty(); q.pop() ){
+        for(q.push({0, 0 , 0, k}) ; !q.empty(); q.pop()){
             auto t = q.front();
-            int x = t[0], y =  t[1];
+            int x = t[0], y = t[1];
             
-            if(x < 0 || y < 0 || x >= m || y >= n) 
+            if(x < 0 || y < 0 || x >= m || y >= n)
                 continue;
             
-            if(x == m - 1 && y == n - 1)
-                return t[3];
+            if(x == m - 1 && y == n-1)
+                return t[2];
             
             if(grid[x][y]){
-                if(t[2] > 0) t[2]--;
+                if(t[3]) t[3]--;
                 else continue;
             }
             
-            if(vis[x][y] != -1 && vis[x][y] >= t[2])
+            if(vis[x][y] != -1 && vis[x][y] >= t[3])
                 continue;
-            vis[x][y] = t[2];
+            vis[x][y] = t[3];
             
-            q.push({x+1, y, t[2], t[3] + 1});
-            q.push({x, y+1, t[2], t[3] + 1});
-            q.push({x-1, y, t[2], t[3] + 1});
-            q.push({x, y-1, t[2], t[3] + 1});
+            t[2]++;
+            q.push({x + 1, y, t[2], t[3]});
+            q.push({x , y + 1, t[2], t[3]});
+            q.push({x - 1, y, t[2], t[3]});
+            q.push({x , y - 1, t[2], t[3]});
+            
+            
+            
         }
         
         return -1;
+        
+        
     }
 };
