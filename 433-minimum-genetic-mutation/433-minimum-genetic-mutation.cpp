591fad1b907a14271038526b843ccdfe921e800d
Shivam Shukla <19ucs040@lnmiit.ac.in>
2022-11-02 09:52:17 +0530

    Time: 5 ms (18.63%), Space: 6.7 MB (42.79%) - LeetHub

--- 433-minimum-genetic-mutation/433-minimum-genetic-mutation.cpp
+++ 433-minimum-genetic-mutation/433-minimum-genetic-mutation.cpp
@@ -3,33 +3,35 @@ public:
     int diff(string &a, string &b){
         if(a== b) return 0;
         int ans = 0;
-        for(int i =0; i < 8; i++)
+        for(int i =0; i < 8 && ans < 2; i++)
             ans += (a[i] != b[i]);
         return ans;  
     }
-    int solve(string start, string end, set<string>& b){
-        if(start == end) return 0;
-        int ans = 1e9;
-
-        set<string> cpy(b.begin(), b.end());
-        for(string s: cpy){
-            if(diff(start, s) == 1){
-                string temp = s;
-                b.erase(temp);
-                int curr = 1 + solve(temp, end, b);
-                ans = min(ans, curr);
-                b.insert(temp);
-            }
-        }
-        return ans;
-    }
     int minMutation(string start, string end, vector<string>& bank) {
-        set<string> b;
-        for(string s: bank){
-            b.insert(s);
-        }
+        unordered_set<string> st(bank.begin(), bank.end());
+        if(!st.count(end)) return -1;
         
-        int ans = solve(start, end, b);
-        return ans >= 1e9 ? -1 : ans;
+        queue<string> q;
+        int steps = 0;
+    
+        for(q.push(start); !q.empty();){
+            int size = q.size();
+            for(int i = 0; i < size; i++, q.pop()){
+                
+                string curr = q.front();
+                
+                if(curr == end) return steps;
+                
+                st.erase(curr);
+                
+                for(string s: st)
+                    if(diff(curr, s) == 1)
+                        q.push(s);
+                
+            }
+            steps++;
+        }
+        return -1;
+        
     }
 };
