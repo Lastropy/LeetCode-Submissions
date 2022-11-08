753389f8f50d1ce1bc3cf7a532936bcc3bdccfdc
Shivam Shukla <19ucs040@lnmiit.ac.in>
2022-11-08 11:41:34 +0530

    Time: 10 ms (20.25%), Space: 6.4 MB (54.48%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.cpp
+++ 1544-make-the-string-great/1544-make-the-string-great.cpp
@@ -1,15 +1,28 @@
 class Solution {
 public:
     string makeGood(string s) {
-        if(s.size() == 1) return s;
-        int p1, p2;
-        for(p1 = 0, p2 = 0; p2 < s.size(); p2++){
-            if(p1 > 0 && abs(s[p1 - 1] - s[p2]) == 32)
-                --p1;
+//         if(s.size() == 1) return s;
+//         int p1, p2;
+//         for(p1 = 0, p2 = 0; p2 < s.size(); p2++){
+//             if(p1 > 0 && abs(s[p1 - 1] - s[p2]) == 32)
+//                 --p1;
+//             else
+//                 s[p1++] = s[p2];
+//         }
+        
+//         return s.substr(0, p1);
+        stack<char> st;
+        for(int i =0; i < s.size(); i++){
+            if(!st.empty() && abs(st.top() - s[i]) == 32)
+                st.pop();
             else
-                s[p1++] = s[p2];
+                st.push(s[i]);
         }
         
-        return s.substr(0, p1);
+        s = "";
+        while(!st.empty())
+            s += st.top(), st.pop();
+        reverse(s.begin(), s.end());
+        return s;
     }
 };
