dca1452786a09858245f7cf6284259bed6c3523d
Shivam Shukla <19ucs040@lnmiit.ac.in>
2022-11-17 20:38:54 +0530

    Time: 7275 ms (12.76%), Space: 27.8 MB (79.08%) - LeetHub

--- 2296-design-a-text-editor/2296-design-a-text-editor.py
+++ 2296-design-a-text-editor/2296-design-a-text-editor.py
@@ -1,44 +1,40 @@
 class TextEditor:
 
     def __init__(self):
-        self.left = []
-        self.right = []
+        self.s=""
+        self.c=0
+        
 
     def addText(self, text: str) -> None:
-        self.left.append(text)
-
-    def _cursor_left(self, k: int, add_position: Optional[bool] = None) -> str:
-        n_chars = 0
-        tokens = []
-        while (remaining := k - n_chars) > 0 and self.left:
-            s = self.left.pop()
-            if len(s) > remaining:
-                self.left.append(s[:-remaining])
-                tokens.append(s[-remaining:])
-                break
-            else:
-                tokens.append(s)
-                n_chars += len(s)
-        result = "".join(tokens[::-1])
-        if result:
-            if add_position is True:
-                self.right.append(result[::-1])
-            elif add_position is False:
-                self.left.append(result)
-        return result
-    
-    def deleteText(self, k: int) -> int:
-        result = len(self._cursor_left(k))
-        return result
+        self.s=self.s[:self.c]+text+self.s[self.c:]
+        self.c+=len(text)
         
+
+    def deleteText(self, k: int) -> int:
+        k=min(len(self.s[:self.c]),k)
+        self.s=self.s[:self.c-k]+self.s[self.c:]
+        self.c-=k
+        return k
+
     def cursorLeft(self, k: int) -> str:
-        self._cursor_left(k, add_position=True)
-        result = self._cursor_left(10, add_position=False)
-        return result
+        while k>0 and self.c>0:
+            self.c-=1
+            k-=1
+        x=min(10,len(self.s[:self.c]))
+        return self.s[self.c-x:self.c]
 
     def cursorRight(self, k: int) -> str:
-        self.left, self.right = self.right, self.left
-        self._cursor_left(k, add_position=True)
-        self.left, self.right = self.right, self.left
-        result = self._cursor_left(10, add_position=False)
-        return result
+        while k>0 and self.c<len(self.s):
+            self.c+=1
+            k-=1
+        x=min(10,len(self.s[:self.c]))
+        return self.s[self.c-x:self.c]
+        
+
+
+# Your TextEditor object will be instantiated and called as such:
+# obj = TextEditor()
+# obj.addText(text)
+# param_2 = obj.deleteText(k)
+# param_3 = obj.cursorLeft(k)
+# param_4 = obj.cursorRight(k)
