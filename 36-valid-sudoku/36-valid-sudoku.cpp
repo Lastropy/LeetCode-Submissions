ed276f350d54b46ca3a9324e94330b5556583ec4
Shivam Shukla <19ucs040@lnmiit.ac.in>
2022-11-23 09:59:24 +0530

    Time: 8 ms (99.92%), Space: 18.9 MB (54.63%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.cpp
+++ 36-valid-sudoku/36-valid-sudoku.cpp
@@ -6,6 +6,7 @@ public:
         
         for(int i = 0; i < 9; i++){
             for(int j = 0; j < 9; j++){
+                // checking row and sub-square
                 if(isdigit(board[i][j])){
                     int key = ((i / 3) * 3 + (j / 3));
                     int val = board[i][j] - '0';
@@ -13,6 +14,7 @@ public:
                         return false;
                     s1[val] = s3[key][val] = 1;
                 }
+                // checking column
                 if(isdigit(board[j][i])){
                     int val2 = board[j][i] - '0';
                     if(s2[val2])
