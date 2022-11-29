f6adaec3ae439957fd48ba04979fa995f3f3eb39
Shivam Shukla <19ucs040@lnmiit.ac.in>
2022-11-29 09:27:10 +0530

    Time: 653 ms (19.98%), Space: 97.2 MB (19.16%) - LeetHub

---
+++ 380-insert-delete-getrandom-o1/380-insert-delete-getrandom-o1.cpp
@@ -0,0 +1,43 @@
+class RandomizedSet {
+private:
+    set<int> s;
+    int size;
+    
+public:
+    RandomizedSet() {
+        size = 0;
+    }
+    
+    bool insert(int val) {
+        if(s.find(val) != s.end())
+            return false;
+        s.insert(val);
+        size++;
+        return true;
+    }
+    
+    bool remove(int val) {
+        if(s.find(val) == s.end())
+            return false;
+        s.erase(val);
+        size--;
+        return true;
+    }
+    
+    int getRandom() {
+        // srand(time(0));
+        
+        int pos = (rand()) % size;
+        auto it = s.begin();
+        advance(it, pos);
+        return (*it);
+    }
+};
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * RandomizedSet* obj = new RandomizedSet();
+ * bool param_1 = obj->insert(val);
+ * bool param_2 = obj->remove(val);
+ * int param_3 = obj->getRandom();
+ */
