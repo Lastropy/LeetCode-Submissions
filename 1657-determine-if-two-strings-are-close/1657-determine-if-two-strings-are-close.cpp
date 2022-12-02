0f5fec90b568b784c2352fbc54b4549cdc21e362
Shivam Shukla <67953167+Lastropy@users.noreply.github.com>
2022-12-02 10:18:02 +0530

    Time: 393 ms (12.28%), Space: 20.7 MB (32.93%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
@@ -5,28 +5,21 @@ public:
             return false;
         
         vector<int> mp1(26, 0), mp2(26, 0);
-        for(char c: word1)
+        set<char> s1, s2;
+        for(char c: word1){
             mp1[c - 'a']++;
-        for(char c: word2)
-            mp2[c  - 'a']++;
-        
-        vector<int> prob1, prob2;
-        for(int i =0; i < 26; i++){
-            if(!mp1[i] && !mp2[i]) 
-                continue;
-            else if(!mp1[i] && mp2[i])
-                return false;
-            else if(mp1[i] && !mp2[i])
-                return false;
-            else if(mp1[i] != mp2[i])
-                prob1.push_back(mp1[i]), prob2.push_back(mp2[i]);
+            s1.insert(c);
         }
-        sort(begin(prob1), end(prob1));
-        sort(begin(prob2), end(prob2));
+        for(char c: word2){
+            mp2[c  - 'a']++;
+            s2.insert(c);
+        }
+        sort(begin(mp1), end(mp1));
+        sort(begin(mp2), end(mp2));
         
         
         
         
-        return prob1 == prob2;
+        return ((mp1 == mp2 )&& (s1 == s2));
     }
 };
