e593bf2c370dd3abb53078228fde52bef5344a12
Shivam Shukla <67953167+Lastropy@users.noreply.github.com>
2022-12-10 10:30:17 +0530

    Time: 4 ms (89.38%), Space: 9.7 MB (97.26%) - LeetHub

--- 1026-maximum-difference-between-node-and-ancestor/1026-maximum-difference-between-node-and-ancestor.cpp
+++ 1026-maximum-difference-between-node-and-ancestor/1026-maximum-difference-between-node-and-ancestor.cpp
@@ -1,20 +1,31 @@
 class Solution {
 public:
-    vector<int> min_max(TreeNode* root, int& res){
-        if(!root) return {(int)1e5, (int)-1e5};
-        
-        auto ans1 = min_max(root -> left, res), ans2 = min_max(root -> right, res);
-        
-        int gmin = min({ans1[0], ans2[0]}), gmax = max({ans1[1], ans2[1]});
-        
-        if(gmin != 1e5)  res = max(res, abs(gmin - (root -> val)));
-        if(gmax != -1e5) res = max(res, abs(gmax - (root -> val)));
-        
-        return vector<int>({min(gmin, root -> val), max(gmax, root -> val)});
-    }
-    int maxAncestorDiff(TreeNode* root) {
-        int res = -1e5;
-        min_max(root, res);
-        return res; 
-    }
+//         vector<int> min_max(TreeNode* root, int& res){
+//             if(!root) return {(int)1e5, (int)-1e5};
+
+//             auto ans1 = min_max(root -> left, res), ans2 = min_max(root -> right, res);
+
+//             int gmin = min({ans1[0], ans2[0]}), gmax = max({ans1[1], ans2[1]});
+
+//             if(gmin != 1e5)  res = max(res, abs(gmin - (root -> val)));
+//             if(gmax != -1e5) res = max(res, abs(gmax - (root -> val)));
+
+//             return vector<int>({min(gmin, root -> val), max(gmax, root -> val)});
+//         }
+//         int maxAncestorDiff(TreeNode* root) {
+//             int res = -1e5;
+//             min_max(root, res);
+//             return res; 
+//         }
+        int dfs(TreeNode* root, int mx, int mn){
+            if(!root) return mx - mn;
+            
+            mx = max(mx, root -> val);
+            mn = min(mn, root -> val);
+            
+            return max(dfs(root -> left, mx, mn), dfs(root -> right, mx, mn));
+        }
+        int maxAncestorDiff(TreeNode* root) {
+            return dfs(root, 0, 100000);
+        }
 };
