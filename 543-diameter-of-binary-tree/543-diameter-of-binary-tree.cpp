a9ff0f68a0d0ba75f4bca36bb2ce7674feed86b0
Shivam Shukla <67953167+Lastropy@users.noreply.github.com>
2022-12-12 18:14:23 +0530

    Time: 15 ms (76.00%), Space: 20.4 MB (14.72%) - LeetHub

---
+++ 543-diameter-of-binary-tree/543-diameter-of-binary-tree.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int dfs(TreeNode *root, int &ans){
+        if(!root) return 0;
+        
+        int lmax = dfs(root -> left, ans);
+        int rmax = dfs(root -> right, ans);
+        
+        ans = max(ans, lmax + rmax + 1);
+        
+        return 1 + max(lmax, rmax);
+    }
+    int diameterOfBinaryTree(TreeNode* root) {
+        int ans = 0;
+        if(root) dfs(root,ans);
+        return ans - 1;
+    }
+};
