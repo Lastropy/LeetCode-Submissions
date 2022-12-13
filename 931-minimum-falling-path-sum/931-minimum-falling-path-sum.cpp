a13a6eb6066fa4e92d177b1a529308144a5fc333
Shivam Shukla <67953167+Lastropy@users.noreply.github.com>
2022-12-13 10:22:45 +0530

    Time: 16 ms (89.50%), Space: 10.3 MB (27.68%) - LeetHub

---
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int solve(vector<vector<int>> &mat, int row, int col, vector<vector<int>> &dp){
+        int n = mat.size();
+        if(row == n) return 0;
+        if(col >= n || col < 0) return 1e9;
+        if(dp[row][col] != -1e9) return dp[row][col];
+        
+        int ans = mat[row][col] + min({solve(mat, row+1, col, dp),
+                                       solve(mat, row+1, col-1, dp),
+                                       solve(mat, row + 1, col + 1, dp)});
+        
+        return dp[row][col] = ans;
+        
+    }
+    
+    int minFallingPathSum(vector<vector<int>>& matrix) {
+        int ans = 1e9;
+        int n = matrix.size();
+        vector<vector<int>> dp(n, vector<int>(n, -1e9));
+        for(int i = 0; i < n; i++)
+            ans = min(ans, solve(matrix, 0, i, dp));
+        return ans;
+    }
+};
