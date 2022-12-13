4193c4159ac75f45de50017ac5ba4176d996691d
Shivam Shukla <67953167+Lastropy@users.noreply.github.com>
2022-12-13 10:24:48 +0530

    Time: 18 ms (84.15%), Space: 10.3 MB (54.12%) - LeetHub

--- 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
-    int solve(vector<vector<int>> &mat, int row, int col, vector<vector<int>> &dp){
-        int n = mat.size();
+    int solve(vector<vector<int>> &mat, int &n, int row, int col, vector<vector<int>> &dp){
         if(row == n) return 0;
         if(col >= n || col < 0) return 1e9;
         if(dp[row][col] != -1e9) return dp[row][col];
         
-        int ans = mat[row][col] + min({solve(mat, row+1, col, dp),
-                                       solve(mat, row+1, col-1, dp),
-                                       solve(mat, row + 1, col + 1, dp)});
+        int ans = mat[row][col] + min({solve(mat, n, row+1, col, dp),
+                                       solve(mat, n, row+1, col-1, dp),
+                                       solve(mat, n, row + 1, col + 1, dp)});
         
         return dp[row][col] = ans;
         
@@ -19,7 +18,7 @@ public:
         int n = matrix.size();
         vector<vector<int>> dp(n, vector<int>(n, -1e9));
         for(int i = 0; i < n; i++)
-            ans = min(ans, solve(matrix, 0, i, dp));
+            ans = min(ans, solve(matrix, n, 0, i, dp));
         return ans;
     }
 };
