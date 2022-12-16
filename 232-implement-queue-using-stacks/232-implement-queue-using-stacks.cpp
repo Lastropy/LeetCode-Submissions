1e00f7ea0fd203cd7b34b532fea6b25f7f7261b2
Shivam Shukla <67953167+Lastropy@users.noreply.github.com>
2022-12-16 16:22:37 +0530

    Time: 0 ms (100.00%), Space: 6.9 MB (88.90%) - LeetHub

--- 232-implement-queue-using-stacks/232-implement-queue-using-stacks.cpp
+++ 232-implement-queue-using-stacks/232-implement-queue-using-stacks.cpp
@@ -12,16 +12,16 @@ public:
     }
     
     int pop() {
+        if(q2.empty() && q1.empty()) return -1;
         if(q2.empty()) transfer(q1, q2);
-        if(q2.empty()) return -1;
         int ans = q2.top();
         q2.pop();
         return ans;
     }
     
     int peek() {
+        if(q2.empty() && q1.empty()) return -1;
         if(q2.empty()) transfer(q1, q2);
-        if(q2.empty()) return -1;
         return q2.top();
     }
     
@@ -29,12 +29,3 @@ public:
         return (q2.empty() && q1.empty());
     }
 };
-
-/**
- * Your MyQueue object will be instantiated and called as such:
- * MyQueue* obj = new MyQueue();
- * obj->push(x);
- * int param_2 = obj->pop();
- * int param_3 = obj->peek();
- * bool param_4 = obj->empty();
- */
