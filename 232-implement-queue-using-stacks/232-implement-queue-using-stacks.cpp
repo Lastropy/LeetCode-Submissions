d33345ff67d63223a226b079359830a56c12990f
Shivam Shukla <67953167+Lastropy@users.noreply.github.com>
2022-12-16 16:23:34 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (54.50%) - LeetHub

--- 232-implement-queue-using-stacks/232-implement-queue-using-stacks.cpp
+++ 232-implement-queue-using-stacks/232-implement-queue-using-stacks.cpp
@@ -1,7 +1,6 @@
 class MyQueue {
 private: 
     void transfer(stack<int> &s, stack<int> &t){
-        if(!t.empty()) return;
         for(;!s.empty(); s.pop())
             t.push(s.top());
     }
@@ -12,7 +11,7 @@ public:
     }
     
     int pop() {
-        if(q2.empty() && q1.empty()) return -1;
+        if(empty()) return -1;
         if(q2.empty()) transfer(q1, q2);
         int ans = q2.top();
         q2.pop();
@@ -20,7 +19,7 @@ public:
     }
     
     int peek() {
-        if(q2.empty() && q1.empty()) return -1;
+        if(empty()) return -1;
         if(q2.empty()) transfer(q1, q2);
         return q2.top();
     }
