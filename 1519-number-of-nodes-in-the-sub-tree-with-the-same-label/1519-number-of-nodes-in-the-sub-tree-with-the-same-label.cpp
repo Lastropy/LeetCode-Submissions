fbaf47194ad5ebac4790cb529ef8b356027c570d
Shivam Shukla <67953167+Lastropy@users.noreply.github.com>
2023-01-12 07:26:10 +0530

    Time: 757 ms (65.19%), Space: 237.9 MB (28.15%) - LeetHub

--- 1519-number-of-nodes-in-the-sub-tree-with-the-same-label/1519-number-of-nodes-in-the-sub-tree-with-the-same-label.cpp
+++ 1519-number-of-nodes-in-the-sub-tree-with-the-same-label/1519-number-of-nodes-in-the-sub-tree-with-the-same-label.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
     
-    vector<int> dfs(int node, vector<bool> &vis, vector<vector<int>> &g, string &labels, vector<int> &ans){
-        if(vis[node]) return {};
+    vector<int> dfs(int node, vector<vector<int>> &g, string &labels, vector<int> &ans){
+        if(ans[node]) return {};
         
-        vis[node] = true;
         vector<int> freq(26, 0);
+        ans[node] = freq[labels[node]-'a'] = 1;
+        
         for(auto nodes: g[node]){
-            auto temp = dfs(nodes,vis, g, labels, ans);
+            auto temp = dfs(nodes, g, labels, ans);
             for(int i = 0; i < temp.size(); i++)
                 freq[i] += temp[i];
         }
         
-        ans[node] = freq[labels[node] - 'a'] + 1;
-        freq[labels[node] - 'a']++;
-        vis[node] = false;
+        ans[node] = freq[labels[node] - 'a'];
         return freq;
     }
 
@@ -25,9 +24,8 @@ public:
             g[v[0]].push_back(v[1]);
             g[v[1]].push_back(v[0]);
         }
-        vector<bool> vis(n, false);
-        vector<int> ans(n, -1);
-        dfs(0, vis, g, labels, ans);
+        vector<int> ans(n, 0);
+        dfs(0, g, labels, ans);
         return ans;
         
     }
