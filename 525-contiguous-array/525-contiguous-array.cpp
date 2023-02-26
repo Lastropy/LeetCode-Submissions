cc817e6d888bf76097423fde3c1413940750775a
Shivam Shukla <67953167+Lastropy@users.noreply.github.com>
2023-02-26 16:14:59 +0530

    Time: 145 ms (65.20%), Space: 86.9 MB (9.18%) - LeetHub

--- 525-contiguous-array/525-contiguous-array.cpp
+++ 525-contiguous-array/525-contiguous-array.cpp
@@ -3,16 +3,17 @@ public:
     int findMaxLength(vector<int>& nums) {
         vector<int> prefs(nums.size());
         int n = nums.size();
+        unordered_map< int, int> mp;
+    
+            
         
         prefs[0] = nums[0];
-        for(int i = 1; i  < n; i++)
-            prefs[i] = prefs[i-1] + nums[i];
-        
-        unordered_map< int, int> mp;
         mp[1] = -1;
+        mp[2*prefs[0]] = 0;
         int ans = 0;
-        for(int i = 0; i <n; i++)
+        for(int i = 1; i <n; i++)
         {
+            prefs[i] = prefs[i-1] + nums[i];
             int val = 2 * prefs[i] - i; 
             if(mp.count(val))
                 ans = max(ans, i - mp[val]);
