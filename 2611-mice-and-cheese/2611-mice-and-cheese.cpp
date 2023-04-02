8ab23b729d8477fd3ad964aad53c8d46f0abe9e2
Shivam Shukla <67953167+Lastropy@users.noreply.github.com>
2023-04-02 16:49:57 +0530

    Time: 131 ms (90.91%), Space: 101.8 MB (36.36%) - LeetHub

--- 2611-mice-and-cheese/2611-mice-and-cheese.cpp
+++ 2611-mice-and-cheese/2611-mice-and-cheese.cpp
@@ -1,19 +1,16 @@
 class Solution {
-    #define ll long long
     public:
-        int miceAndCheese(vector<int>& r1, vector<int>& r2, int k) {
-            priority_queue<ll> pq;
-            ll n = r1.size(), ans = 0;
-            for(int i =0; i < n; i++){
-                ans += r2[i];
-                pq.push({r1[i] -r2[i]});
-            }
-
-            while(k){
-                ans += pq.top(), k--; 
-                pq.pop();
-            }
-      
-            return ans;
-        }
+#define ll 
+int miceAndCheese(vector<int>& r1, vector<int>& r2, int k) {
+    long long n = r1.size(), ans = 0;
+    for(int i = 0; i < n; i++){
+        ans += r2[i];
+        r1[i] -= r2[i];
+    }
+    
+    // Finds biggest k elements in array in O(N)
+    nth_element(begin(r1), begin(r1) + k, end(r1), greater<int>());
+    
+    return accumulate(begin(r1), begin(r1)+ k, ans);
+}
 };
