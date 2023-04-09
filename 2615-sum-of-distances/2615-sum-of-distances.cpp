12ce21b35ef66b1a19f1d87f3a5388c10c6bbe5f
Shivam Shukla <67953167+Lastropy@users.noreply.github.com>
2023-04-09 17:05:00 +0530

    Time: 246 ms (83.33%), Space: 109.4 MB (16.67%) - LeetHub

---
+++ 2615-sum-of-distances/2615-sum-of-distances.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<long long> distance(vector<int>& a) {
+        unordered_map<int, vector<int>> mp;
+        int n = a.size();
+        vector<long long> ans(n, 0);
+        
+        for(int i = 0; i < n; i++)
+            mp[a[i]].push_back(i);
+        
+        for(auto [k,v] : mp){
+            if(v.size() == 1) continue;
+            long long left_sum = 0, right_sum = 0, pref_sum_left = 0, pref_sum_right = 0;
+            for(int j = v.size() - 1; j >= 0; j--)
+                pref_sum_right += v[j];
+            
+            for(int j = 0; j < v.size(); j++){
+                left_sum = (1LL * j * v[j] - pref_sum_left);
+                pref_sum_left += v[j];
+                pref_sum_right -= v[j];
+                right_sum = (pref_sum_right - 1LL* v[j] * (v.size() - 1 - j));
+                // cout << v[j] << " => "<<left_sum << " | "<< right_sum << endl;
+                ans[v[j]] = left_sum + right_sum;
+            }
+        }
+        
+        return ans;
+    }
+};
