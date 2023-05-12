2f4e4f2738f3524e48f06a18fa7a3d2097777d80
Shivam Shukla <67953167+Lastropy@users.noreply.github.com>
2023-05-12 18:57:14 +0530

    Time: 408 ms (16.18%), Space: 115.3 MB (84.05%) - LeetHub

--- 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.cpp
+++ 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.cpp
@@ -2,15 +2,13 @@ class Solution {
 public:
     long long mostPoints(vector<vector<int>>& q) {
         int n = q.size();
-        vector<long long> dp(n + 1, 0), ans(n+1, 0);
+        vector<long long> dp(n + 1, 0);
         
         for(int i = n - 1; i >= 0; i--){
-            dp[i+1] = q[i][0] + (i + q[i][1]+ 2 <= n ? ans[i + q[i][1]+ 2]: 0);
-            ans[i+1] = max(dp[i+1], i+2 <= n ? ans[i+2]: 0);
-            // for(int i: dp)
-            //     cout << i << ",";
-            // cout << endl;
+            long long take = q[i][0] + (q[i][1] + i + 1 <=n ? dp[q[i][1] + i + 1]: 0);
+            long long skip = max(dp[i], dp[i+1]);
+            dp[i] = max(take, skip);
         }
-        return ans[1];
+        return dp[0];
     }
 };
