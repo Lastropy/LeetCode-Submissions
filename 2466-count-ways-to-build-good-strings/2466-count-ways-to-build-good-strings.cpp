6e949b4545580050a06a5cd81dad3b7508e459e1
Shivam Shukla <67953167+Lastropy@users.noreply.github.com>
2023-05-13 19:24:31 +0530

    Time: 38 ms (6.02%), Space: 9.3 MB (81.95%) - LeetHub

---
+++ 2466-count-ways-to-build-good-strings/2466-count-ways-to-build-good-strings.cpp
@@ -0,0 +1,33 @@
+class Solution {
+private:
+    vector<int> dp;
+    int mod = 1e9 + 7;
+    int goodStringsWithSizeEqualTo(int k, int &z, int &o){
+        if(k == 0) return 1;
+        if(k< 0 || (k < z && k < o)) return 0;
+        if(dp[k] != -1) return dp[k];
+        
+        int ans = 0;
+        if(k - z >= 0) ans +=  goodStringsWithSizeEqualTo(k-z, z, o);
+        ans %= mod;
+        if(k - o >= 0) ans +=  goodStringsWithSizeEqualTo(k-o, z, o);
+        ans %= mod;
+        return dp[k] = ans; 
+    }
+public:
+    int countGoodStrings(int l, int h, int z, int o) {
+        dp = vector<int>(h+1, -1);
+        dp[0] = 1;
+          
+        int ans = 0;
+        for(int i = 1; i <= h; i++){
+            goodStringsWithSizeEqualTo(i, z, o);
+            if(i >= l){
+                ans += dp[i];
+                ans %= mod;
+            }
+        }
+        return ans;
+        
+    }
+};
