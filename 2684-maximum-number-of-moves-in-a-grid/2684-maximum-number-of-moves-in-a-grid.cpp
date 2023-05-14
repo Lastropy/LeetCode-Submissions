b8da7e46520dfaaa9acce5f47073b27b691c63aa
Shivam Shukla <67953167+Lastropy@users.noreply.github.com>
2023-05-14 15:33:24 +0530

    Time: 234 ms (69.23%), Space: 69.1 MB (23.08%) - LeetHub

--- 2684-maximum-number-of-moves-in-a-grid/2684-maximum-number-of-moves-in-a-grid.cpp
+++ 2684-maximum-number-of-moves-in-a-grid/2684-maximum-number-of-moves-in-a-grid.cpp
@@ -1,31 +1,28 @@
 class Solution {
 private:
     vector<vector<int>> dp;
-    int dfs(vector<vector<int>>& grid, int r, int c, int &m, int &n){
+    int dfs(vector<vector<int>>& grid, int r, int c){
+        int m = grid.size(), n = grid[0].size();
         if(c >= n || r < 0 || r >= m || c < 0) return 0;
         if(dp[r][c]!= -1) return dp[r][c];
-        
         int ans = 0;
-        
-        if(c < n - 1 && grid[r][c+1] > grid[r][c])
-            ans = max(ans, 1+ dfs(grid, r, c+1, m , n));
-        
-        if(r && c < n - 1 && grid[r-1][c+1] > grid[r][c] )
-            ans = max(ans, 1+ dfs(grid, r-1, c+1, m , n));
-        
-        if(r < m - 1 && c < n - 1 && grid[r+1][c+1] > grid[r][c] )
-            ans = max(ans, 1+ dfs(grid, r+1, c+1, m , n));
-        
+        if( c + 1 < n && grid[r][c+1] > grid[r][c])
+            ans = max(ans, 1+ dfs(grid, r, c+1));
+        if(r && c+ 1 < n&&  grid[r-1][c+1] > grid[r][c] )
+            ans = max(ans, 1+ dfs(grid, r-1, c+1));
+        if(r < grid.size()-1 && c+ 1< n && grid[r+1][c+1] > grid[r][c] )
+            ans = max(ans, 1+ dfs(grid, r+1, c+1));
         return dp[r][c] = ans;
     }
 public:
     int maxMoves(vector<vector<int>>& grid) {
-        int ans= 0, m = grid.size(), n = grid[0].size();
+        int m = grid.size(), n = grid[0].size();
+        int ans= 0;
         dp = vector<vector<int>>(m, vector<int>(n, -1));
-        
-        for(int i =0; i < m; i++)
-            ans = max(ans,dfs(grid,i,0, m, n));
-        
+        for(int i =0; i < grid.size(); i++){
+            ans = max(ans,dfs(grid,i,0));
+            if(ans == grid[0].size() - 1) break;
+        }
         return ans;
     }
 };
