bd74d3e1927460824fe7879ac48395016ea1571b
Shivam Shukla <67953167+Lastropy@users.noreply.github.com>
2023-09-24 09:36:55 +0530

    Time: 8 ms (55.02%), Space: 14.8 MB (37.29%) - LeetHub

--- 0799-champagne-tower/0799-champagne-tower.cpp
+++ 0799-champagne-tower/0799-champagne-tower.cpp
@@ -1,7 +1,7 @@
 class Solution {
 private:
-    int tar_row;
     void equate(int r, vector<vector<double>> &dp){
+        if(r > dp.size() - 2) return;
         for(int i = 0; i <= r; i++){
             if(dp[r][i] >= 1){
                 double excess = dp[r][i] -1;
@@ -10,12 +10,11 @@ private:
                 dp[r][i] = 1;
             }
         }
-        if(r < tar_row) equate(r + 1, dp);
+        equate(r + 1, dp);
     }
 public:
     double champagneTower(int poured, int r, int g) {
-        vector<vector<double>> dp(101, vector<double>(101, 0.00));
-        tar_row = r;
+        vector<vector<double>> dp(r+2, vector<double>(r+2, 0.00));
         dp[0][0] = poured;
         equate(0, dp);
         return dp[r][g];
