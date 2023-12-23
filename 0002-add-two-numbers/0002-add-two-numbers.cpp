a8a75b5c25a5c1bc73bc8e5e1cada57e1cde4055
Shivam Shukla <67953167+Lastropy@users.noreply.github.com>
2023-12-23 06:52:02 +0530

    Time: 20 ms (68.47%), Space: 71.9 MB (25.45%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -1,57 +1,31 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        if(!l1 || !l2) return l1? l1: l2;
-        
-        int carry =0;
-        auto ans = new ListNode();
+        auto a = l1;
+        auto b = l2;
+        int carry = 0;
+        ListNode* ans = new ListNode(-1);
         auto ans2 = ans;
-        while(l1 && l2){
-            l1 -> val += l2 -> val + carry;
-            carry = (l1 -> val) / 10;
-            l1 -> val %= 10;
-            ans -> next = l1;
-            ans = ans -> next;
-            l1 = l1 -> next;
-            l2 = l2 -> next;
-        }
-        
-        if(l1){
-            ans -> next = l1;
-            while(carry && l1){
-                l1 -> val += carry;
-                carry = (l1 -> val) / 10;
-                l1 -> val %= 10;
-                ans -> next = l1;
-                ans = ans -> next;
+        while(l1 || l2){
+            ans -> next = new ListNode(carry);
+            if(l1){
+                ans -> next -> val += l1 -> val;
                 l1 = l1 -> next;
             }
+            if(l2){
+                ans -> next -> val += l2 -> val;
+                l2 = l2 -> next;
+            }
+            carry = (ans -> next -> val) / 10;
+            ans -> next -> val = (ans -> next -> val) % 10;
+            ans = ans -> next;
         }
         
-         if(l2){
-             ans -> next = l2;
-            while(carry && l2){
-                l2 -> val += carry;
-                carry = (l2 -> val) / 10;
-                l2 -> val %= 10;
-                ans -> next = l2;
-                ans = ans -> next;
-                l2 = l2-> next;
-
-            }
-        }
-        if(carry){
+        
+        while(carry){
             ans -> next = new ListNode(carry);
+            carry = (ans -> next -> val) / 10;
+            ans -> next -> val = (ans -> next -> val) % 10;
         }
         return ans2 -> next;
     }
