5f0160762aeef2672db42940f6589e0497aa4ec6
Shivam Shukla <67953167+Lastropy@users.noreply.github.com>
2023-12-23 12:21:19 +0530

    Added solution - LeetHub

--- Reverse a Linked List in groups of given size - GFG/reverse-a-linked-list-in-groups-of-given-size.cpp
+++ Reverse a Linked List in groups of given size - GFG/reverse-a-linked-list-in-groups-of-given-size.cpp
@@ -30,38 +30,45 @@ void printList(struct node *node)
 // } Driver Code Ends
 class Solution
 {
+    private:
+    struct node* rev(struct node *h){
+        if(!h || !h -> next) return h;
+        struct node* prev = NULL;
+        auto curr = h;
+        auto nxt = h -> next;
+        while(curr){
+            curr -> next = prev;
+            prev = curr;
+            curr = nxt;
+            if(nxt) nxt = nxt -> next;
+        }
+        return prev;
+    }
     public:
-    struct node *reverse (struct node *head, int k)
+    struct node *reverseIt (struct node *h, int k)
     { 
-        if(k == 1 || !head) return head;
-        auto curr = head;
-        for(int i = 1; i < k && curr; i++)
-            curr = curr -> next;
-        if(curr){
-            auto temp = reverse(curr -> next, k);
-            curr -> next = NULL;
-            struct node* prev = NULL;
-            auto curr = head, nxt = head -> next;
-            while(curr){
-                curr -> next = prev;
-                prev = curr;
-                curr = nxt;
-                if(nxt) nxt = nxt -> next;
-            }
-            head -> next = temp;
-            return prev;
+        if(!h || k == 1) return h;
+        struct node* first = h;
+        struct node* prev = NULL;
+        while(first){
+           auto last = first;
+           for(int i = 1; i < k && last; i++) 
+                last = last -> next;
+           if(!last){
+               if(prev) prev -> next = rev(first);
+               else h = rev(first);
+               break;
+           }
+           auto nxt = last -> next;
+           last -> next = NULL;
+           last = rev(first);
+           if(prev == NULL) h = last;
+           else prev -> next = last;
+           first -> next = nxt;
+           prev = first;
+           first = nxt;
         }
-        else {
-            struct node* prev = NULL;
-            auto curr = head, nxt = head -> next;
-            while(curr){
-                curr -> next = prev;
-                prev = curr;
-                curr = nxt;
-                if(nxt) nxt = nxt -> next;
-            }
-            return prev;
-        }
+        return h;
     }
 };
 
@@ -101,7 +108,7 @@ int main(void)
         cin>>k;
         
         Solution ob;
-        head = ob.reverse(head, k);
+        head = ob.reverseIt(head, k);
         printList(head);
     }
      
