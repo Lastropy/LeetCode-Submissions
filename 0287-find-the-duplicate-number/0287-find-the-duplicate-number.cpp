0e54edb1cd9de91366b419dbcbb581e209d3aebe
Shivam Shukla <67953167+Lastropy@users.noreply.github.com>
2023-12-25 15:22:02 +0530

    Time: 89 ms (50.24%), Space: 61.5 MB (89.99%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -1,17 +1,33 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& a) {
-        for(int i = 0; i < a.size(); i++){
-            while(a[i] != i+1 && a[a[i] - 1] != a[i]){
-                swap(a[i], a[a[i]-1]);
+        int n = a.size();
+        // for(int i = 0; i < n; i++){
+        //     while(a[i] != i+1 && a[a[i] - 1] != a[i]){
+        //         swap(a[i], a[a[i]-1]);
+        //     }
+        // }
+        // for(int i = 0; i < n; i++){
+        //     if(a[i] != i+1){
+        //         return a[i];
+        //     }
+        // }
+        // return -1;
+        int ans = -1;
+        for(int i = 0; i < n; i++){
+            int idx = a[i] < 0 ? -1 * a[i] : a[i];
+            idx--;
+            if(a[idx] > 0)
+                a[idx] *= -1;
+            else{
+                ans = idx + 1;
+                break;
             }
         }
-        for(int i = 0; i < a.size(); i++){
-            if(a[i] != i+1){
-                return a[i];
-            }
+        for(int i = 0; i < n; i++){
+            a[i] = (a[i] < 0? -a[i]: a[i]); 
         }
-        return -1;
+        return ans;
         
     }
 };
