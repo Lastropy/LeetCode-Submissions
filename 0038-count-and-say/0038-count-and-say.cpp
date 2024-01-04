d51ca6f2999ae3441e6ab3ce2bf39f0bade0d416
Shivam Shukla <67953167+Lastropy@users.noreply.github.com>
2024-01-04 20:19:03 +0530

    Time: 3 ms (70.42%), Space: 7.1 MB (59.47%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.cpp
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -1,23 +1,26 @@
 class Solution {
 public:
+    string countNsay(string s){
+        string temp = "";
+        char prev = s[0];
+        int count = 0;
+        for(int j = 0; j < s.size(); j++){
+            if(prev == s[j]){
+                count++;
+            } else {
+                temp += string(1, count+'0') + string(1, prev);
+                count = 1;
+                prev = s[j];
+            }
+        }
+        temp += string(1, count+'0') + string(1, prev);
+        return temp;
+    }
+    
     string countAndSay(int n) {
         string ans = "1";
-        for(int i = 2; i <= n; i++){
-            string temp = "";
-            char prev = ans[0];
-            int count = 0;
-            for(int j = 0; j < ans.size(); j++){
-                if(prev == ans[j]){
-                    count++;
-                } else {
-                    temp += string(1, count+'0') + string(1, prev);
-                    count = 1;
-                    prev = ans[j];
-                }
-            }
-            temp += string(1, count+'0') + string(1, prev);
-            ans = temp;
-        }
+        for(int i = 2; i <= n; i++)
+            ans = countNsay(ans);
         return ans;
     }
 };
