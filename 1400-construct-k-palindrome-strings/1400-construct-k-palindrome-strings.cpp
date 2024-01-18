f962f5b38aa09be4e62afb6c1c1f5c2c831faf3e
Shivam Shukla <67953167+Lastropy@users.noreply.github.com>
2024-01-18 07:30:14 +0530

    Time: 37 ms (41.53%), Space: 12.4 MB (31.97%) - LeetHub

--- 1400-construct-k-palindrome-strings/1400-construct-k-palindrome-strings.cpp
+++ 1400-construct-k-palindrome-strings/1400-construct-k-palindrome-strings.cpp
@@ -5,39 +5,13 @@ public:
         if(k > n) return false;
         if(k == n) return true;
         unordered_map<char, int> mp;
-        for(char c: s)
-            mp[c]++;
-        
-        int buckets = mp.size();
-        if(buckets <= k) return true;
+        for(char c: s) mp[c]++;
+        if(mp.size() <= k) return true;
         int eveners = 0, odders = 0;
         for(auto [k, v]: mp){
             if(v % 2 == 0) eveners++;
             else odders++;
         }
-        if(eveners == odders) return true;
         return k >= odders;       
-        
-        /*
-            abcd = {
-                a: 1
-                b: 1
-                c: 1
-                d: 1
-            }
-            x1, x2, x3
-            
-            abbd = {
-                a: 1
-                b: 2
-                d: 1
-            }
-            x1, 2,
-            e  = 1
-            o  = 2
-        
-        
-        
-        */
     }
 };
