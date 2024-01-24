aad126c9e92aaabe93a49eebd9f102d4eb1f279f
Shivam Shukla <67953167+Lastropy@users.noreply.github.com>
2024-01-24 07:35:17 +0530

    Time: 4 ms (37.24%), Space: 12.1 MB (5.98%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -1,7 +1,15 @@
 class Solution {
 public:
-    int searchInsert(vector<int>& nums, int target) {
-        int ans = lower_bound(nums.begin(), nums.end(), target) - nums.begin();
-        return ans;
+    int searchInsert(vector<int>& a, int t) {
+        int n = a.size();
+        
+        int l = 0, h = n - 1;
+        while(l <= h){
+            int m = l + (h -l)/2;
+            if(a[m] == t) return m;
+            else if(a[m] < t) l = m + 1;
+            else h = m - 1;
+        }
+        return l;
     }
 };
