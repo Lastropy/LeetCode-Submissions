aafba3854d712ff222075b049ab93bf4fb66b8ae
Shivam Shukla <67953167+Lastropy@users.noreply.github.com>
2024-01-24 07:29:57 +0530

    Time: 35 ms (20.74%), Space: 29.9 MB (5.49%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -4,12 +4,9 @@ public:
         int l = 0, h = nums.size() - 1;
         while(l <= h){
             int mid = l + (h - l)/2;
-            if(nums[mid] == target)
-                return mid;
-            else if(nums[mid] < target)
-                l = mid + 1;
-            else
-                h = mid - 1;  
+            if(nums[mid] == target) return mid;
+            else if(nums[mid] < target) l = mid + 1;
+            else h = mid - 1;  
         }
         return -1;
     }
