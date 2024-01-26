76159881d1691a526dc3b12fe6fdd090986ab1ea
Shivam Shukla <67953167+Lastropy@users.noreply.github.com>
2024-01-26 16:34:16 +0530

    Time: 7 ms (9.67%), Space: 11.3 MB (8.59%) - LeetHub

---
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int findPeakElement(vector<int>& a) {
+        int n = a.size(), s = 0, e = n - 1;
+        while(s<=e){
+            int m = s + (e - s) /2;
+            if((m == 0 || a[m-1] < a[m]) && (m == n - 1 || a[m+1] < a[m]))
+                return m;
+            else if((m && (a[m-1] > a[m])) || (m < n - 1 && (a[m + 1] < a[m])))
+                e = m - 1;
+            else s = m + 1;
+        }
+        return (s % n);
+    }
+};
