f157db3c788a219cbaa064a9ba06030bf240840e
Shivam Shukla <67953167+Lastropy@users.noreply.github.com>
2024-01-28 16:57:23 +0530

    Time: 0 ms (100.00%), Space: 7.3 MB (6.54%) - LeetHub

--- 0400-nth-digit/0400-nth-digit.cpp
+++ 0400-nth-digit/0400-nth-digit.cpp
@@ -1,46 +1,57 @@
 class Solution {
 public:
-    int findNthDigit(int n) {
-        // // 100th digit
-        // // [1, 100]
-        // // 35 -> 3 => ?? , 5 => ??
-        // int s = 1, e = n;
-        // while(s <= e){
-        //     int m = s + (e-s)/2;
-        //     if()
-        // } 
-        // return;
+    int lengthOfNumber(int n){
+        int length = 0;
+        while(n){
+            length++;
+            n/=10;
+        }
+        return length;
+    }
+    
+    long long int digitsOccured(int n){
+        if(n == 0) return 0;
+        int digNum = lengthOfNumber(n);
+        long long int len = 1, base = 9, count = 0;
         
-        
-        
-        
-        
-        //linear search
-        // [1-9] : {9 - 1 + 1} * 1 => 9 digits => 9 * 1
-        // [10 - 99] : {99 - 10 + 1} * 2 => 180 digits => 90 * 2
-        // [100 - 999]: {999 - 100 + 1} * 3 => 2700 digits. => 900 * 3
-        // [1000 - 9999] : {9999 - 1000 + 1} * 4 => 36000 digits => 9000 * 4
-        
-        // Step - 1: Find number of digits in our answer;
-        
-        int base = 9;
-        int len = 1;
-        int curr = 1;
-        while(n > 1LL * base * len){
-            n -= base * len;
+        while(len < digNum)
+        {
+            count += base * len;
             len++;
             base *= 10;
-            curr *= 10;
         }
-        curr += ((n - 1)/len);
-        int pos_of_dig = len - ((n-1)% len);
+        count += (n - pow(10, len - 1) + 1) * (len);
+        return count;
+    }
+    
+    int findNthDigit(int n) {
+        // binary search
+        // min answer possible = 1
+        // max answer possible = n
+        int s = 1, e = n, mid;
+        
+        // Step - 1: Pick a number in range;
+        while(s <= e){
+            mid = s + (e-s)/2;
+            
+            // Step - 2: Find Number of digits that have occured till (mid - 1)
+            long long int occured = digitsOccured(mid-1);
+            int length = lengthOfNumber(mid);
+            // cout << (mid) << " | " << length << " | " << occured << endl;
+             
+            if(occured >= n) e = mid - 1;
+            else if(occured < n && occured + length >= n){
+                n = length - (n - occured);
+                break;
+            }
+            else s = mid + 1;
+        }
+        
         int ans = -1;
-        while(pos_of_dig){
-            ans = curr % 10;
-            curr /= 10;
-            pos_of_dig--;
+        for(int i = 0; i <= n; i++){
+            ans = mid % 10;
+            mid /= 10;
         }
-        // cout << curr << " | "<< pos_of_dig << endl;
         return ans;
     }
 };
