3e12487df6c6d5d088c7341e2279bb37ee597f2f
Shivam Shukla <67953167+Lastropy@users.noreply.github.com>
2024-02-06 20:44:01 +0530

    Time: 8 ms (8.87%), Space: 13.4 MB (23.81%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,26 +1,34 @@
 class Solution {
 public:
-    int find_rotation_index(vector<int> &a){
-        int s = 0, e = a.size() - 1;
-        while(s <= e){
-            int m = s + (e- s)/2;
-            if(a[m] >= a[0]) s = m + 1;
-            else e = m - 1;
-        }
-        return e;
-    }
-     
-    int find_occ(vector<int> &a, int tar, int s, int e){
-        while(s <= e){
-            int m = s + (e-s)/2;
-            if(a[m] == tar) return m;
-            else if(a[m] < tar) s = m + 1;
-            else e = m - 1;
-        }
-        return - 1;
-    }
     int search(vector<int>& a, int t) {
-        int point_of_rot = find_rotation_index(a);
-        return max(find_occ(a, t, 0, point_of_rot), find_occ(a, t, point_of_rot + 1, a.size() - 1));
+        int n = a.size();
+        int s = 0, e = n - 1;
+        while(s <= e){
+            int m = s + (e - s)/2;
+            // if element found
+            if(a[m] == t) return m;
+            
+            // left half is sorted
+            if(a[s] <= a[m]){
+                // element present in left half
+                if(a[s] <= t && t <= a[m]){
+                    e = m - 1;
+                }
+                // element not present in left half
+                else s = m + 1;
+            } 
+            
+            // right half is sorted
+            else {
+                // element present in right half
+                if(a[m] <= t && t <= a[e]){
+                    s = m + 1;
+                }
+                // element not present in right half
+                else e = m - 1;
+            }
+        }
+        // element not found
+        return -1;
     }
 };
