3bd28291925eec1982e70bd06751c34b8680a27a
Shivam Shukla <67953167+Lastropy@users.noreply.github.com>
2024-02-07 20:48:33 +0530

    Time: 2 ms (44.47%), Space: 7.8 MB (35.17%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -1,19 +1,14 @@
 class Solution {
-public:
-    int ncr(int n, int r) {
-        int ans = 1;
-        for(int i = 1; i <= r; i++){
-            ans *= (n - i + 1);
-            ans /= i;
-        }
-        return ans;
-    }
-    
+public:    
     vector<vector<int>> generate(int n) {
         vector<vector<int>> ans(n);
         for(int r = 1; r <= n; r++) {
-            for(int c = 1; c <= r; c++) {
-                ans[r - 1].push_back(ncr(r - 1, c - 1));
+            int ele = 1;
+            ans[r - 1].push_back(ele);
+            for(int c = 1; c < r; c++) {
+                ele *= (r - c);
+                ele /= c;
+                ans[r - 1].push_back(ele);
             }
         }
         return ans;
