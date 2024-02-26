755110b43dfa3b57ad527fe2f25dbd0ed7705706
Shivam Shukla <67953167+Lastropy@users.noreply.github.com>
2024-02-26 07:04:15 +0530

    Time: 51 ms (35.00%), Space: 33.5 MB (41.06%) - LeetHub

---
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool canShip(vector<int>& a, int mid, int t){
+        int ans =0, curr = 0;
+        for(int i = 0; i < a.size(); i++){
+            if(a[i] > mid) return false;
+            if(curr + a[i] <= mid){
+                curr += a[i];
+            } else {
+                ans++;
+                curr = a[i];
+            }
+        }
+        return ((ans + 1) <= t);
+    }
+    
+    int shipWithinDays(vector<int>& w, int t) {
+        int s = 1, e = INT_MAX;       
+        int ans = -1;
+        while(s <= e){
+            int mid = s + (e - s)/2;
+            if(canShip(w, mid, t)){
+                ans = mid;
+                e = mid - 1;
+            } else {
+                s = mid + 1;
+            }
+        }
+        return ans;
+    }
+};
