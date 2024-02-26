d4d295de05726ce06b5f34d2f3f3b7db0f852ab9
Shivam Shukla <67953167+Lastropy@users.noreply.github.com>
2024-02-26 06:24:40 +0530

    Time: 101 ms (95.63%), Space: 69 MB (21.74%) - LeetHub

---
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    bool canHarvest(int mid, vector<int>& g, int m, int k){
+        int ans = 0, curr = 0;
+        for(int i = 0; i < g.size(); i++){
+            if(g[i] <= mid){
+                curr++;
+            } else {
+                curr = 0;
+            }
+            if(curr == k) {
+                curr = 0;
+                ans++;
+            }
+        }
+        return (ans >= m);
+    }
+    
+    int minDays(vector<int>& g, int m, int k) {
+        int n = g.size();
+        if(1LL * m * k > n) return -1;
+        int s = INT_MAX, e = INT_MIN;
+        for(int i: g){
+            s = min(i, s);
+            e = max(i, e);
+        }
+        int ans = -1;
+        while(s <= e){
+            int mid = s + (e - s)/2;
+            if(canHarvest(mid, g, m, k)){
+                ans = mid;
+                e = mid - 1;
+            } else {
+                s = mid + 1;
+            }
+        }
+        return ans;
+    }
+};
