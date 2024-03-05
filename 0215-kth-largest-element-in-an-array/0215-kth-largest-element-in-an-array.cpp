56f7a436e621e2069698e3c95a724332f7a212c2
Shivam Shukla <67953167+Lastropy@users.noreply.github.com>
2024-03-05 08:00:34 +0530

    Time: 98 ms (52.51%), Space: 57.5 MB (91.13%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -1,8 +1,35 @@
 class Solution {
 public:
+    int partition(vector<int> &arr, int lo, int hi){
+        int pEle = arr[hi], s = lo, e = hi - 1;
+        while(s <= e){
+            if (arr[s] < pEle && arr[e] > pEle) {
+                swap(arr[s++], arr[e--]);
+            }
+            if (arr[s] >= pEle) {
+                s++;
+            }
+            if (arr[e] <= pEle) {
+                e--;
+            }
+        }
+        swap(arr[hi], arr[s]);
+        return s;
+    }
+    
     int findKthLargest(vector<int>& arr, int k) {
         int n = arr.size();
-        nth_element(arr.begin(), arr.begin() + k - 1, arr.end(), greater<int>());
-        return arr[k - 1];
+        int lo = 0, hi = n - 1;
+        while(true){
+            int pos = partition(arr, lo, hi);
+            if(pos == k - 1){
+                return arr[pos];
+            } else if(pos < k - 1){
+                lo = pos + 1;
+            } else {
+                hi = pos - 1;
+            }
+        }
+        return -1;
     }
 };
