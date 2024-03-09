6410a3f3b612708b4f3e1a4b20b5f0b103516a24
Shivam Shukla <67953167+Lastropy@users.noreply.github.com>
2024-03-09 11:53:10 +0530

    Time: 128 ms (53.37%), Space: 76.6 MB (45.82%) - LeetHub

--- 2462-total-cost-to-hire-k-workers/2462-total-cost-to-hire-k-workers.cpp
+++ 2462-total-cost-to-hire-k-workers/2462-total-cost-to-hire-k-workers.cpp
@@ -6,12 +6,10 @@ public:
         priority_queue<int, vector<int>, greater<int>> pq1, pq2;
         int pos1 =0, pos2 = n - 1;
         while(k){
-            if(pos1 <= pos2 && pq1.size() < c) {
+            if(pos1 <= pos2 && pq1.size() < c) 
                 pq1.push(a[pos1++]);
-            }
-            else if(pos1 <= pos2 && pq2.size() < c){
+            else if(pos1 <= pos2 && pq2.size() < c)
                 pq2.push(a[pos2--]);
-            }
             else {
                 if(pq1.empty() && pq2.empty()) return ans;
                 else if(pq1.empty()) {
