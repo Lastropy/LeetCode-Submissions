ade76618a1973c43324bd64b84a3b87310b5be2a
Shivam Shukla <67953167+Lastropy@users.noreply.github.com>
2024-03-15 10:02:58 +0530

    Time: 209 ms (37.81%), Space: 76.1 MB (87.80%) - LeetHub

--- 2580-count-ways-to-group-overlapping-ranges/2580-count-ways-to-group-overlapping-ranges.cpp
+++ 2580-count-ways-to-group-overlapping-ranges/2580-count-ways-to-group-overlapping-ranges.cpp
@@ -9,19 +9,20 @@ public:
         return res;
     }
     int countWays(vector<vector<int>>& a) {
+        if(a.size() == 0) return 1;
         sort(a.begin(), a.end(), [&](const vector<int> &p, const vector<int> &q){
             return p[0] < q[0];
         });
         
-        int grps = 1, curr = 0, curr_max = a[0][1], ans = 1;
+        int grps = 1, curr = 0, curr_max = a[0][1], ans = 2;
         for(int i = 1; i < a.size(); i++){
             if(a[i][0] > curr_max){
                 grps++;
                 curr_max = a[i][1];
-                ans = (ans *2 ) % mod;
+                ans = (ans * 2) % mod;
             }
             curr_max = max(curr_max, a[i][1]);
         }
-        return permut(grps);
+        return ans;
     }
 };
