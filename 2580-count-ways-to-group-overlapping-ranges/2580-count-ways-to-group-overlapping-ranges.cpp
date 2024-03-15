d545dff6862e3af51d34cbf754f67fe361207bc4
Shivam Shukla <67953167+Lastropy@users.noreply.github.com>
2024-03-15 10:02:10 +0530

    Time: 195 ms (59.35%), Space: 76 MB (87.80%) - LeetHub

--- 2580-count-ways-to-group-overlapping-ranges/2580-count-ways-to-group-overlapping-ranges.cpp
+++ 2580-count-ways-to-group-overlapping-ranges/2580-count-ways-to-group-overlapping-ranges.cpp
@@ -13,11 +13,12 @@ public:
             return p[0] < q[0];
         });
         
-        int grps = 1, curr = 0, curr_max = a[0][1];
+        int grps = 1, curr = 0, curr_max = a[0][1], ans = 1;
         for(int i = 1; i < a.size(); i++){
             if(a[i][0] > curr_max){
                 grps++;
                 curr_max = a[i][1];
+                ans = (ans *2 ) % mod;
             }
             curr_max = max(curr_max, a[i][1]);
         }
