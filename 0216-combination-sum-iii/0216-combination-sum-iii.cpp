ab637781da325e9975512c138abe4bd9b1494e0e
Shivam Shukla <67953167+Lastropy@users.noreply.github.com>
2024-04-02 07:14:39 +0530

    Time: 0 ms (100.00%), Space: 8.6 MB (30.63%) - LeetHub

---
+++ 0216-combination-sum-iii/0216-combination-sum-iii.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    void solve( vector<vector<int>> &ans, vector<int> &curr, int k, int t, int currDig = 1){
+        if(k == curr.size()) {
+            if(t == 0) ans.push_back(curr);     
+            return;
+        }
+        if(currDig >= 10) return;
+        curr.push_back(currDig);
+        solve(ans, curr, k, t - currDig, currDig + 1);
+        curr.pop_back();
+        solve(ans, curr, k, t, currDig + 1);
+    }
+    
+    vector<vector<int>> combinationSum3(int k, int n) {
+        vector<vector<int>> ans;
+        vector<int> curr;
+        solve(ans, curr,k , n);
+        return ans;
+    }
+};
