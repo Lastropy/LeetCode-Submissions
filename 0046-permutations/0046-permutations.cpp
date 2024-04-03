37a21d48039e71da5fc65f1ff8fa3664f7bcfefa
Shivam Shukla <67953167+Lastropy@users.noreply.github.com>
2024-04-03 06:49:17 +0530

    Time: 4 ms (41.93%), Space: 8.6 MB (97.60%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,27 +1,21 @@
 class Solution {
 public:
-    void solve(vector<vector<int>> &ans, unordered_map<int, int> &mp,        vector<int> &a, vector<int> &curr){
-        if(curr.size() == a.size()) {
-            ans.push_back(curr);
+    void solve(vector<vector<int>> &ans, vector<int> &a, int idx){
+        if(idx == a.size()) {
+            ans.push_back(a);
             return;
         }
         
-        for(int i = 0; i < a.size(); i++){
-            if(mp[a[i]] == 0) {
-                mp[a[i]] = 1;
-                curr.push_back(a[i]);
-                solve(ans, mp, a, curr);
-                mp[a[i]] = 0;
-                curr.pop_back();
-            }
+        for(int i = idx; i < a.size(); i++){
+            swap(a[idx], a[i]);
+            solve(ans, a, idx + 1);
+            swap(a[idx], a[i]);
         }
     }
     
     vector<vector<int>> permute(vector<int>& a) {
         vector<vector<int>> ans;
-        unordered_map<int, int> mp;
-        vector<int> curr;
-        solve(ans, mp, a, curr);
+        solve(ans, a, 0);
         return ans;
     }
 };
