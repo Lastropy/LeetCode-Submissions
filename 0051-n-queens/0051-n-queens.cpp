b7f6f52af3a199ebd426c278201fd598f85c7b53
Shivam Shukla <67953167+Lastropy@users.noreply.github.com>
2024-04-05 07:00:40 +0530

    Time: 4 ms (63.12%), Space: 11.5 MB (33.27%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,58 +1,46 @@
 class Solution {
 public:
-    bool canPlaceQueen(vector<string> &board, int r, int c){
-        int cpyR = r;
-        int cpyC = c;
-        
-        while(r >= 0 && c >= 0){
-            if(board[r][c] == 'Q'){
-                return false;
-            }
-            r--;
-            c--;
+    bool canPlaceQueen(vector<bool> &rows, vector<bool> &upper_diagonal, vector<bool> &lower_diagonal, int r, int c){
+        if(rows[r] == true){
+            return false;
         }
         
-        r = cpyR;
-        c = cpyC;
-        
-        while(r < board.size() && c >= 0){
-            if(board[r][c] == 'Q'){
-                return false;
-            }
-            r++;
-            c--;
+        if(lower_diagonal[r + c] == true){
+            return false;
         }
         
-        r = cpyR;
-        c = cpyC;
-        
-        while(c >= 0){
-            if(board[r][c] == 'Q'){
-                return false;
-            }
-            c--;
+        if(upper_diagonal[rows.size() - 1 + r - c] == true){
+            return false;
         }
         return true;
     }
     
-    void solve(vector<vector<string>> &ans, vector<string> &board, int c = 0){
+    void solve(vector<vector<string>> &ans, vector<string> &board, vector<bool> &rows, vector<bool> &upper_diagonal, vector<bool> &lower_diagonal, int c = 0){
         if(c == board.size()){
             ans.push_back(board);
             return;
         }
         
         for(int r = 0; r < board.size(); r++){
-            if(canPlaceQueen(board, r, c)){
+            if(canPlaceQueen(rows, upper_diagonal, lower_diagonal, r, c)){
                 board[r][c] = 'Q';
-                solve(ans, board, c+1);
+                rows[r] = true;
+                upper_diagonal[(board.size() -1) + (r - c)] = true;
+                lower_diagonal[r + c] = true;
+                solve(ans, board,rows, upper_diagonal, lower_diagonal, c+1);
                 board[r][c] = '.';
+                rows[r] = false;
+                upper_diagonal[(board.size() -1) + (r - c)] = false;
+                lower_diagonal[r + c] = false;
             }
         }
     }
+    
     vector<vector<string>> solveNQueens(int n) {
         vector<vector<string>> ans;
+        vector<bool> rows(n, false), upper_diagonal(2 *n -1, false), lower_diagonal(2 *n - 1, false);
         vector<string> board(n, string(n, '.'));
-        solve(ans, board);
+        solve(ans, board, rows, upper_diagonal, lower_diagonal);
         return ans;
     }
 };
