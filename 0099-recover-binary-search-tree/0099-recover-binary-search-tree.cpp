8f7df94010b16cabfd6488dfbf97825e6bc03317
Shivam Shukla <67953167+Lastropy@users.noreply.github.com>
2024-08-13 19:04:52 +0530

    Time: 21 ms (58.07%), Space: 60.7 MB (51.73%) - LeetHub

--- 0099-recover-binary-search-tree/0099-recover-binary-search-tree.cpp
+++ 0099-recover-binary-search-tree/0099-recover-binary-search-tree.cpp
@@ -1,16 +1,13 @@
 class Solution {
 private: 
-    TreeNode *prev = NULL, *first = NULL, *sec = NULL, *middle = NULL;
+    TreeNode *prev = NULL, *first = NULL, *sec = NULL;
 public:
     void inorder(TreeNode *root){
         if(!root) return;
         inorder(root -> left);
         if(prev && (prev -> val) >= (root -> val)) {
-            if(first) sec = root;
-            else {
-                first = prev;
-                middle = root;
-            }
+            if(!first) first = prev;
+            sec = root;
         }
         prev = root;
         inorder(root -> right);
@@ -18,6 +15,5 @@ public:
     void recoverTree(TreeNode* root) {
         inorder(root);
         if(first && sec) swap(first -> val, sec -> val);
-        else if(first && middle) swap(first -> val, middle -> val);
     }
 };
