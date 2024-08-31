5dc042c472102af0e4f2311e7593d23303a981a6
Shivam Shukla <67953167+Lastropy@users.noreply.github.com>
2024-08-31 15:40:30 +0530

    Time: 0 ms (100.00%), Space: 16.1 MB (80.25%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -2,49 +2,50 @@ class Solution {
 public:
     int orangesRotting(vector<vector<int>>& g) {
         queue<pair<int, int>> q;
-        int n = g.size(), m = g[0].size();
-        bool fresh = false;
+        int n = g.size(), m = g[0].size(), cntFresh = 0;
+        
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
-                if(g[i][j] == 2){
-                    q.push({i, j});
-                }
-                else if(g[i][j] == 1) fresh = true;
+                if(g[i][j] == 2) q.push({i, j});
+                else if(g[i][j] == 1) cntFresh++;
             }
         }
-        if(!fresh) return 0;
+        if(!cntFresh) return 0;
         
-        int ans = -1;
+        int ans = 0, cntRotten = 0;
         while(!q.empty()){
             ans++;
             int siz = q.size();
             for(int i = 0; i < siz; i++){
                 auto tp = q.front(); q.pop();
                 int x = tp.first, y = tp.second;
+                
                 if(x + 1 < n && g[x+1][y] == 1){
                     g[x+1][y] = 2;
+                    cntRotten++;
                     q.push({x+1, y});
                 }
+                
                 if(y + 1 < m && g[x][y+1] == 1){
                     g[x][y+1] = 2;
+                    cntRotten++;
                     q.push({x, y+1});
                 }
+                
                 if(x - 1 >= 0 && g[x-1][y] == 1){
                     g[x-1][y] = 2;
+                    cntRotten++;
                     q.push({x-1, y});
                 }
+                
                 if(y - 1 >= 0 && g[x][y - 1] == 1){
                     g[x][y-1] = 2;
+                    cntRotten++;
                     q.push({x, y-1});
                 }
             }
         }
-        
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
-                if(g[i][j] == 1) return -1;
-            }
-        }
-        return ans;
+        if(cntFresh != cntRotten) return - 1;
+        return (ans - 1);
     }
 };
