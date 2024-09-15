2e8f17eb714016b6e69bdc6d2b99a375645593d3
Shivam Shukla <67953167+Lastropy@users.noreply.github.com>
2024-09-15 07:08:44 +0530

    Time: 67 ms (66.82%), Space: 19.5 MB (88.72%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.cpp
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -1,58 +1,32 @@
 class Solution {
 public:
-    int diff(string &s1, string &s2){
-        int len = s1.size();
-        int diff = 0;
-        for(int i = 0; i < len && diff < 2; i++){
-            if(s1[i] != s2[i]) diff++;
-        }
-        return diff;
-    }
-    
-    int ladderLength(string b, string e, vector<string>& a) {
+   int ladderLength(string b, string e, vector<string>& a) {
         int n = a.size(), m = b.size();
-        int pos_src = -1, pos_dest = -1;
-        for(int i =0; i < n; i++){
-            if(a[i] == b) pos_src = i;
-            if(a[i] == e) pos_dest = i;
-        }
-        if(pos_dest == -1) return 0;
-        if(pos_src == -1){
-             a.push_back(b);
-             pos_src = n++;
-        }
-        
-        vector<vector<int>> adj(n, vector<int>(n, 0));
-        
-        for(int i = 0; i < n; i++){
-            for(int j = i+1; j < n; j++){
-                if(diff(a[i],a[j]) == 1){
-                    adj[i][j] = 1;
-                    adj[j][i] = 1;
-                }
-            }
-        }
-        
-        queue<int> q;
-        q.push(pos_src);
-        vector<int> dist(n, 1e9);
-        dist[pos_src] = 0;
-        
+        unordered_set<string> st(a.begin(), a.end());
+        st.erase(b);        
+        queue<string> q;
+        q.push(b);   
+        int ans = 1;
         while(!q.empty()){
             int siz = q.size();
+            ans++;
             for(int i = 0; i< siz; i++){
                 auto tp = q.front(); q.pop();
-                for(int j = 0; j < n; j++){
-                    if(adj[tp][j]){
-                        if(dist[j] > dist[tp] + 1){
-                            dist[j] = dist[tp] + 1;
-                            q.push(j);
+                for(int j = 0; j < m; j++){
+                    char org = tp[j];
+                    for(char c = 'a'; c <= 'z'; c++){
+                        tp[j] = c;
+                        if(st.find(tp) != st.end()){
+                            if(tp == e) return ans;
+                            q.push(tp);
+                            st.erase(tp);
                         }
                     }
+                    tp[j] = org;
                 }
             }
         }
-        return dist[pos_dest] >= 1e9 ? 0 : dist[pos_dest] + 1;
+        return 0;
         
     }
 };
