f0604d595dea3190d006e6e1cc2a2cd91a266caf
Shivam Shukla <67953167+Lastropy@users.noreply.github.com>
2024-09-20 06:21:07 +0530

    Time: 81 ms (62.87%), Space: 47.4 MB (37.91%) - LeetHub

---
+++ 0743-network-delay-time/0743-network-delay-time.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
+        vector<pair<int, int>> adj[n+1];
+        for(auto t: times){
+            adj[t[0]].push_back({t[1], t[2]});
+        }
+        vector<long long> dist(n+1,LLONG_MAX);
+        priority_queue<pair<long long, int>, vector<pair<long long, int>>, greater<pair<long long, int>>> pq;
+        pq.push({0, k});
+        dist[k] = 0;
+        while(!pq.empty()){
+            auto tp = pq.top(); pq.pop();
+            int node = tp.second;
+            long long curr_dist = tp.first;
+            for(auto u: adj[node]){
+                int child = u.first, edW = u.second;
+                if(dist[child] > curr_dist + edW){
+                    dist[child] = curr_dist + edW;
+                    pq.push({dist[child], child});
+                }
+            }
+        }
+        long long ans = 0;
+        for(int i = 1; i < dist.size(); i++){
+            if(dist[i] == LLONG_MAX) {
+                cout << i << endl;
+                return -1;
+            }
+            ans = max(dist[i] , ans);
+        }
+        return ans;
+    }
+};
