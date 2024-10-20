ea812140ec2aa06d06506be49505bb7480b9a931
Shivam Shukla <67953167+Lastropy@users.noreply.github.com>
2024-10-20 12:25:10 +0530

    Time: 174 ms (38.01%), Space: 43.9 MB (18.50%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,13 +1,22 @@
 class Solution {
 public:
+    map<pair<int, int>, int> dp;
     int solve(vector<int>& a, int idx, int t) {
-        if(idx == a.size()) return (t == 0);
-        int add = solve(a, idx + 1, t + a[idx]);
-        int subtract = solve(a, idx + 1, t - a[idx]);
-        return add + subtract;
+        if(idx == a.size()){
+            return (t == 0);
+        }
+        pair<int, int> p = {idx, t};
+        if(dp.count(p)) return dp[p];
+        int notTake = solve(a, idx + 1, t);
+        int take = 0;
+        if(t >= a[idx]) take = solve(a, idx + 1, t - a[idx]);
+        return dp[p] = take + notTake;
     }
     
     int findTargetSumWays(vector<int>& a, int t) {
-        return solve(a, 0, t);
-    }
+        int totalSum = accumulate(a.begin(), a.end(), 0);
+        if((totalSum + t) % 2) return 0;
+        int tar = (totalSum + t) / 2;
+        return solve(a, 0, tar);
+     }
 };
