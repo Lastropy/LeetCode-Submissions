febad5c6975f1269fd8dee46dc194a3c36703b89
Shivam Shukla <67953167+Lastropy@users.noreply.github.com>
2024-10-23 15:07:54 +0530

    Time: 443 ms (40.73%), Space: 220.8 MB (35.90%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
-    int f(vector<int> &p, int idx, bool buy, int lim, vector<vector<vector<int>>> &dp){
+    vector<vector<vector<int>>> dp;
+    int f(vector<int> &p, int idx, bool buy = true, int lim = 2){
         if(lim == 0 || idx == p.size()) return 0; 
         if(dp[idx][buy][lim] != -1) return dp[idx][buy][lim];
         if(buy){
-            int b = -p[idx] + f(p, idx + 1, 0, lim, dp);
-            int nb = 0 + f(p, idx + 1, 1, lim, dp);
+            int b = -p[idx] + f(p, idx + 1, 0, lim);
+            int nb = 0 + f(p, idx + 1, 1, lim);
             return dp[idx][buy][lim] = max(b, nb);
         }
         else{
-            int s = p[idx] + f(p, idx+1, 1, lim-1, dp);
-            int ns = 0 + f(p, idx+1, 0, lim, dp);
+            int s = p[idx] + f(p, idx+1, 1, lim-1);
+            int ns = 0 + f(p, idx+1, 0, lim);
             return dp[idx][buy][lim] = max(s, ns);
         }
     }
     int maxProfit(vector<int>& p) {
         int n = p.size();
-        vector<vector<vector<int>>> dp(n, vector<vector<int>>(2, vector<int>(3, -1)));
-        return f(p, 0, 1, 2, dp);
+        dp.resize(n, vector<vector<int>>(2, vector<int>(3, -1)));
+        return f(p, 0);
     }
 };
