6d6c70cca463a969d6333b0fe2c7827c89f4a931
Shivam Shukla <67953167+Lastropy@users.noreply.github.com>
2024-10-23 14:39:17 +0530

    Time: 0 ms (100.00%), Space: 96.3 MB (24.39%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,22 +1,12 @@
 class Solution {
 public:
-    vector<vector<int>> dp;
-    int solve(vector<int> &p, int idx, bool bought = false) {
-        if(idx == p.size()) return 0;
-        if(dp[idx][bought] != -1e9) return dp[idx][bought];
-        if(!bought) {
-            int buy = -p[idx] + solve(p, idx + 1, !bought);
-            int notBuy = 0 + solve(p, idx + 1, bought);
-            return dp[idx][bought] = max(buy, notBuy);
-        } else {
-            int sell = +p[idx];
-            int notSell = solve(p, idx + 1, bought);
-            return dp[idx][bought] = max(sell, notSell);
-        }
-    }
-    
     int maxProfit(vector<int>& p) {
-        dp.resize(p.size(), vector<int>(2, -1e9));
-        return solve(p, 0);
+        int ans = 0;
+        int mini = p[0];
+        for(int i = 1; i < p.size(); i++){
+            ans = max(ans, p[i] - mini);
+            mini = min(mini,  p[i]);
+        }
+        return ans;
     }
 };
