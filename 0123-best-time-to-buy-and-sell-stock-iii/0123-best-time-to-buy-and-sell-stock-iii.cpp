8034f70d57b1f68ec2d49776c12a6c4317f02ea5
Shivam Shukla <67953167+Lastropy@users.noreply.github.com>
2024-10-23 15:06:48 +0530

    Time: 431 ms (46.17%), Space: 222.5 MB (14.86%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,21 +1,18 @@
 class Solution {
 public:
     int f(vector<int> &p, int idx, bool buy, int lim, vector<vector<vector<int>>> &dp){
-        int n = p.size();
-        if(lim == 0 || idx == n) return 0; 
+        if(lim == 0 || idx == p.size()) return 0; 
         if(dp[idx][buy][lim] != -1) return dp[idx][buy][lim];
-        int ans = 0;
         if(buy){
             int b = -p[idx] + f(p, idx + 1, 0, lim, dp);
             int nb = 0 + f(p, idx + 1, 1, lim, dp);
-            ans = max(b, nb);
+            return dp[idx][buy][lim] = max(b, nb);
         }
         else{
             int s = p[idx] + f(p, idx+1, 1, lim-1, dp);
             int ns = 0 + f(p, idx+1, 0, lim, dp);
-            ans = max(s, ns);
+            return dp[idx][buy][lim] = max(s, ns);
         }
-        return dp[idx][buy][lim] = ans;
     }
     int maxProfit(vector<int>& p) {
         int n = p.size();
