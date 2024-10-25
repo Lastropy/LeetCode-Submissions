6ae0860dde4f002ba8fa5ccaf3a5c62e073e7ec3
Shivam Shukla <67953167+Lastropy@users.noreply.github.com>
2024-10-25 10:56:09 +0530

    Time: 54 ms (74.47%), Space: 13.2 MB (68.23%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -2,19 +2,14 @@ class Solution {
 public:
     int lengthOfLIS(vector<int>& a) {
         int n = a.size();
-        vector<int> next(n+1, 0);
-        for(int idx = n-1; idx >= 0; idx--){
-            vector<int> curr(n+1, 0);
-            for(int prev_idx = idx - 1; prev_idx >= -1; prev_idx--){
-                int notTake = 0 + next[prev_idx + 1];
-                int take = 0;
-                if(prev_idx == -1 || a[idx] > a[prev_idx]){
-                    take = 1 + next[idx + 1];
-                }
-                curr[prev_idx + 1] = max(take, notTake);
-            }
-            next = curr;
+        vector<int> dp(n, 1);
+        for(int idx = 0; idx < n; idx++){
+           for(int prev_idx = 0; prev_idx < idx; prev_idx++){
+               if(a[idx] > a[prev_idx]){
+                   dp[idx] = max(dp[idx], 1 + dp[prev_idx]);
+               }
+           }
         }
-        return next[-1 + 1];
+        return *max_element(dp.begin(), dp.end());
     }
 };
