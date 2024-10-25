6ec84c3917e5eb567aa7ebd0e1b2c6ead29fd45d
Shivam Shukla <67953167+Lastropy@users.noreply.github.com>
2024-10-25 10:45:15 +0530

    Time: 278 ms (29.62%), Space: 293.7 MB (33.18%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
-    vector<vector<int>> dp;
-    int solve(vector<int>& a, int idx, int prev_idx = -1) {
-        if(idx == a.size()) return 0;
-        if(dp[idx][prev_idx + 1] != -1) return dp[idx][prev_idx + 1];
-        int notTake = 0 + solve(a, idx + 1, prev_idx);
-        int take = 0;
-        if(prev_idx == -1 || a[idx] > a[prev_idx]){
-            take = 1 + solve(a, idx + 1, idx);
-        }
-        return dp[idx][prev_idx + 1] = max(take, notTake);
-    }
-    
     int lengthOfLIS(vector<int>& a) {
-        dp.resize(a.size() + 1, vector<int>(a.size() + 1, -1));
-        return solve(a, 0);
+        int n = a.size();
+        vector<int> next(n+1, 0);
+        for(int idx = n-1; idx >= 0; idx--){
+            vector<int> curr(n+1, 0);
+            for(int prev_idx = idx - 1; prev_idx >= -1; prev_idx--){
+                int notTake = 0 + next[prev_idx + 1];
+                int take = 0;
+                if(prev_idx == -1 || a[idx] > a[prev_idx]){
+                    take = 1 + next[idx + 1];
+                }
+                curr[prev_idx + 1] = max(take, notTake);
+            }
+            next = curr;
+        }
+        return next[-1 + 1];
     }
 };
