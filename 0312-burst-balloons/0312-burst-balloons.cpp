b4dac48858179fa5ba582aa142a71c251cd84f85
Shivam Shukla <67953167+Lastropy@users.noreply.github.com>
2024-10-27 08:43:19 +0530

    Time: 202 ms (50.85%), Space: 12.5 MB (7.35%) - LeetHub

---
+++ 0312-burst-balloons/0312-burst-balloons.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int f(vector<int> &a, int i, int j, vector<vector<int>> &dp){
+        if(i > j) return 0;
+
+        if(dp[i][j] != -1)
+            return dp[i][j];
+
+        int ans = INT_MIN;
+
+        for(int k = i; k <= j; k++){
+            int tmp = f(a, i, k-1, dp) + f(a, k+1, j, dp);
+            ans = max(ans, tmp + (a[i-1] * a[k] * a[j+1]));
+        }
+        return dp[i][j] = ans;
+    }
+    
+    int maxCoins(vector<int>& nums) {
+        int n = nums.size();
+        nums.insert(nums.begin() , 1);
+        nums.push_back(1);
+        vector<vector<int>> dp(n+3, vector<int>(n+3, -1));
+        return f(nums, 1, n, dp);
+    }
+};
