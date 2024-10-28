8282c5712964151d4786d155d953726bdf6148cb
Shivam Shukla <67953167+Lastropy@users.noreply.github.com>
2024-10-28 11:23:05 +0530

    Time: 1506 ms (44.11%), Space: 7.9 MB (89.37%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -8,12 +8,12 @@ private:
 public:
     vector<int> dp;
     int solve(string &s, int st){
-        if(st >= s.size() - 1) return 0;
+        if(st == s.size()) return 0;
         if(dp[st] != -1) return dp[st];
         int ans = s.size();
         for(int k = st; k < s.size(); k++){
             if(isPalin(s, st, k)){
-                ans = min(ans, (k != s.size() - 1) + solve(s, k + 1));
+                ans = min(ans, 1 + solve(s, k + 1));
             }
         }
         return dp[st] = ans;
@@ -21,6 +21,7 @@ public:
     
     int minCut(string s) {
         dp.resize(s.size(), -1);
-        return solve(s, 0);
+        // -1 because 1 extra partition is captured -> ab| one
+        return solve(s, 0) - 1;
     }
 };
