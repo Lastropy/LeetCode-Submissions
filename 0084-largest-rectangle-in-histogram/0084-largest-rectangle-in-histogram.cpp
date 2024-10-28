401bd5df8878b7e5f702573377c182bf0ff2e1ba
Shivam Shukla <67953167+Lastropy@users.noreply.github.com>
2024-10-28 16:11:16 +0530

    Time: 16 ms (94.38%), Space: 80 MB (62.54%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,34 +1,25 @@
 class Solution {
 public:
-    int largestRectangleArea(vector<int>& h) {
-        stack<vector<int>> st;
-        int n = h.size();
-        vector<int> ans(n, 0);
+    int largestRectangleArea(vector<int>& a) {
+        stack<int> st;
+        int n = a.size();
+        int ans = -1e9;
         for(int i = 0; i < n; i++){
-            while(!st.empty() && st.top()[0] >= h[i])
-                st.pop();
-            if(st.empty()) {
-                ans[i] += (i + 1) * h[i];
-            } else {
-                ans[i] += (i - (st.top()[1])) * h[i];
+            while(!st.empty() && a[st.top()] > a[i]){
+                int h = a[st.top()], w = 0; st.pop();
+                if(st.empty()) w = i;
+                else w = i - st.top() - 1;
+                ans = max(ans, h * w);
             }
-            st.push({h[i], i});
+            st.push(i);
         }
         
-        while(!st.empty()) st.pop();
-        
-        for(int i = n - 1; i >= 0; i--){
-            while(!st.empty() && st.top()[0] >= h[i])
-                st.pop();
-            if(st.empty()) {
-                ans[i] += (n - 1 - i) * h[i];
-            } else {
-                ans[i] += ((st.top()[1]) - i - 1) * h[i];
-            }
-            st.push({h[i], i});
+        while(!st.empty()){
+            int h = a[st.top()], w = 0; st.pop();
+            if(st.empty()) w = n;
+            else w = n - (st.top()) - 1;
+            ans = max(ans, h * w);
         }
-        
-        return *max_element(ans.begin(), ans.end());
-        
+        return ans;
     }
 };
