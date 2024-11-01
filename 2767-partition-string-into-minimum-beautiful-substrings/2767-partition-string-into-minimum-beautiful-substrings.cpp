7b94f84a3c9aa020da691c3bbea739de1f1c4f9a
Shivam Shukla <67953167+Lastropy@users.noreply.github.com>
2024-11-01 12:16:08 +0530

    Time: 4 ms (38.30%), Space: 10.2 MB (42.96%) - LeetHub

--- 2767-partition-string-into-minimum-beautiful-substrings/2767-partition-string-into-minimum-beautiful-substrings.cpp
+++ 2767-partition-string-into-minimum-beautiful-substrings/2767-partition-string-into-minimum-beautiful-substrings.cpp
@@ -1,25 +1,21 @@
 class Solution {
 public:
     vector<int> dp;
-    int solve(string &s, int len, unordered_set<string>& valid){
-        if(len == 0) return 0;
-        if(dp[len] != -1) return dp[len];
-        int ans = 1e9;
-        for(int idx = len - 1; idx >= 0; idx--){
-            string pos = s.substr(idx, len - idx);
-            if(valid.find(pos) != valid.end()){
-                ans = min(ans, 1 + solve(s, idx, valid));
-            }
-        }
-        return dp[len] = ans;
-    }
-    
     int minimumBeautifulSubstrings(string s) {
-        // '1', '101', '11001', '1111101', '1001110001', '110000110101', '11110100001001'
         unordered_set<string> valid({"1", "101", "11001", "1111101", "1001110001", "110000110101", "11110100001001"});
         int n = s.size();
-        dp.resize(n + 1, -1);
-        int ans = solve(s, n, valid);
+        dp.resize(n + 1, 1e9);
+        dp[0] = 0;
+        if(s[0] == '1') dp[1] = 1;
+        for(int len = 2; len <= n; len++){
+            for(int lastFix = 1; lastFix <= len; lastFix++){
+                string fixed = s.substr(len - lastFix, lastFix);
+                if(valid.find(fixed) != valid.end()){
+                    dp[len] = min(dp[len], 1 + dp[len-lastFix]);
+                }
+            }
+        }
+        int ans = dp[n];
         if(ans >= 1e9) return -1;
         return ans;
     }
